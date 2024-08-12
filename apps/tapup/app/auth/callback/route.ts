08b0c3eb2895ad8ce6fb7b1aea74edccad8fc730
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-12 15:26:56 +0800

    [Feat] sign up callback.

---
+++ apps/tapup/app/auth/callback/route.ts
@@ -0,0 +1,16 @@
+import { createRoute } from "@/utils/supabase"
+import { NextResponse } from "next/server"
+
+import type { NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+
+  if (code) {
+    const supabase = createRoute()
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_BASE_URL}/dashboard`);
+}
