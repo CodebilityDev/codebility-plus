9e4fe2d951f1be0ff0ed9f97677e48af1e2be7da
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-12 17:13:25 +0800

    Feat: login page.

---
+++ apps/tapup/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '@/components/auth/LoginForm'
+
+function Login() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default Login
