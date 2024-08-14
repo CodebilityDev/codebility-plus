f4d84435a7b9d1093687325ca2ae38b2f5b82c02
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-14 20:11:05 +0800

    Feat: logged user page.

---
+++ apps/tapup/app/home/(user)/_components/home-navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React, { useContext } from 'react'
+import NavbarMenu from './home-navbar-menu'
+import { UserWorkspaceContext } from './user-workspace-context'
+
+function HomeNavbar() {
+  const user = useContext(UserWorkspaceContext)
+
+  return (
+    <div className="flex justify-between bg-white">
+      <div></div>
+      <div className="flex items-center gap-x-2 px-6 py-3">
+        <div>Hi, {user.full_name}</div>
+        <div>
+          <NavbarMenu />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeNavbar
---
+++ apps/tapup/app/home/(user)/_components/home-sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function HomeSidebar() {
+  return <div className="h-full bg-black"></div>
+}
+
+export default HomeSidebar
