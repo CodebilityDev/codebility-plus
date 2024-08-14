4311ce7a259f2b52317c0a3faa0e2025b0e5b281
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-14 22:45:49 +0800

    Update: extend custom colors.

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -4,10 +4,22 @@ const { theme } = require('app/design/tailwind/theme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./**/*.{js,jsx,ts,tsx}'],
   theme: {
     ...theme,
+    extend: {
+      colors: {
+        custom: {
+          green: '#22C55E',
+          black: '#1e1e1e',
+          gray: '#ededed',
+          textGray: '#454545',
+          badge: {
+            greenbg: '#bcf6cc',
+            redbg: '#fecdcd',
+          },
+        },
+      },
+    },
   },
 }
