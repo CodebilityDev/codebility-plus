cc39f30dedee57894e34ad37f9dfbaa678d0b4b3
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-14 22:51:29 +0800

    Feat: sign up page.

---
+++ apps/tapup/app/auth/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from '@/components/auth/SignUpForm'
+
+function Register() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default Register
