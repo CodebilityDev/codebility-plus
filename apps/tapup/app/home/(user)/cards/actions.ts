ceef0241e771cc5096ce3b706888cf6244bed137
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 15:38:44 +0800

    feat: send code activation after card creation.

--- apps/tapup/app/home/(user)/cards/actions.ts
+++ apps/tapup/app/home/(user)/cards/actions.ts
@@ -2,17 +2,30 @@
 
 import { cookies } from "next/headers";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { getMailer } from "@codevs/mailers";
+import generateCode from "./_lib/generateCode";
 
-export const createCard = async (user_id: string,name: string, role: string) => {
+export const createCard = async ({id, email}: {id: string, email: string},name: string, role: string) => {
     const supabase = createServerActionClient({ cookies });
 
+    const code = generateCode(4);
+
     const { error, data } = await supabase.from('cards').insert({
-        user_id,
+        user_id: id,
         name,
+        code,
         industry: role,
     });
 
     if (error) throw error;
+
+    const mailer = await getMailer();
+    mailer.sendEmail({
+        to: email,
+        from: process.env.EMAIL_USER as string,
+        subject: "Card Activation",
+        html: `Activate your card with this code: ${code}`
+    });
     
     return data;
 }
