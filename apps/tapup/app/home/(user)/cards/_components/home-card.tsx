f097f491d770006b785a183a469c443ab154d7c5
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 15:46:29 +0800

    feat: activate card modal.

--- apps/tapup/app/home/(user)/cards/_components/home-card.tsx
+++ apps/tapup/app/home/(user)/cards/_components/home-card.tsx
@@ -1,6 +1,7 @@
 import { PenLine } from 'lucide-react'
 import React from 'react'
 import Card from '~/types/cards'
+import HomeActivateCardModal from './home-activate-card-modal'
 
 interface CardProps {
   card: Card
@@ -16,9 +17,14 @@ const HomeCard = ({ card, className, height, width }: CardProps) => {
       className={`flex h-[15vw] w-[24vw] flex-col rounded-[20px] bg-black px-6 pb-6 pt-8 text-white ${className}`}
       style={{ width: width, height: height }}
     >
-      <div className="flex flex-1 flex-col items-end">
-        <p className="text-xs font-bold">{name}</p>
-        <p className="text-[10px]">{industry}</p>
+      <div className="flex  justify-between">
+        <div className="text-xs">
+          <HomeActivateCardModal cardId={id} />
+        </div>
+        <div className="flex flex-1 flex-col items-end">
+          <p className="text-xs font-bold">{name}</p>
+          <p className="text-[10px]">{industry}</p>
+        </div>
       </div>
       <div className="flex items-center justify-between">
         <a href={username_url} className="text-[8px] text-[#b4b4b4]">
