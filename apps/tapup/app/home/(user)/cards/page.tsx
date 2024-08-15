a362d7438b8f1136626eb7754fa5acc4f86c883a
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 13:53:10 +0800

    refactor: remove unnecessary improts.

--- apps/tapup/app/home/(user)/cards/page.tsx
+++ apps/tapup/app/home/(user)/cards/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from '@codevs/ui/button'
 import React from 'react'
-import HomeCard from './_components/home-card'
 import HomeAddCardModal from './_components/home-add-card-modal'
 import HomeCardsContainer from './_components/home-cards-container'
 
