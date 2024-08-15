679b399551c5a17c7974c975811588eef0ce7f9c
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 13:52:27 +0800

    feat: get cards function.

--- apps/tapup/app/home/(user)/cards/actions.ts
+++ apps/tapup/app/home/(user)/cards/actions.ts
@@ -12,5 +12,25 @@ export const createCard = async (user_id: string,name: string, role: string) => 
         industry: role,
     });
 
+    if (error) throw error;
+    
     return data;
 }
+
+export const getCards = async () => {
+    const supabase = createServerActionClient({ cookies });
+
+    const {
+      error: fetchingUserError,
+      data: { user },
+    } = await supabase.auth.getUser()
+
+    if (!user) throw fetchingUserError;
+    
+    const { error, data } = await supabase.from('cards')
+    .select('*')
+    .eq('user_id', user.id);
+
+    if (error) throw error;
+    return data;
+}   
