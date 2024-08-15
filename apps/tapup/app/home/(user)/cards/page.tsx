57b92f6b197b9972777b68000dc375abb15859fd
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 10:10:00 +0800

    feat: cards page.

---
+++ apps/tapup/app/home/(user)/cards/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@codevs/ui/button'
+import React from 'react'
+import HomeCard from './_components/home-card'
+import HomeAddCardModal from './_components/home-add-card-modal'
+
+function CardsPage() {
+  return (
+    <div>
+      <HomeAddCardModal />
+    </div>
+  )
+}
+
+export default CardsPage
