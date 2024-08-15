0f097966a8bae4d8111bff4e01e1134da72b449a
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 15:49:41 +0800

    update: make code a string.

--- apps/tapup/types/cards.ts
+++ apps/tapup/types/cards.ts
@@ -2,7 +2,7 @@ export default interface Card {
     id: string;
     name: string;
     username_url: string;
-    code: number;
+    code: string;
     status: boolean;
     industry: string;
     created_at: string;
