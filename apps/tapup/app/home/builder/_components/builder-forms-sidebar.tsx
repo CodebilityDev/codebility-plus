56904a545e6ae6275058bc47020d8c1317b2c29a
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-16 01:10:35 +0800

    feat: update form after clicking sidebar.

--- apps/tapup/app/home/builder/_components/builder-forms-sidebar.tsx
+++ apps/tapup/app/home/builder/_components/builder-forms-sidebar.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+'use client'
+
 import SidebarItems from '../_lib/builder-form-sidebar-items'
 import { Button } from '@codevs/ui/button'
+import useBuilderForm from '../_hooks/useBuilderForm'
+import { cn } from '@codevs/ui'
 
 function BuilderFormsSidebar() {
+  const { current, updateForm } = useBuilderForm()
   return (
     <div className="flex flex-col gap-y-4">
       {SidebarItems.map((items) => {
+        const { target } = items
         return (
           <Button
-            key={items.target}
-            className="hover:bg-custom-green bg-white text-gray-400 hover:text-white"
+            onClick={() => updateForm(target)}
+            key={target}
+            className={cn(
+              'hover:bg-custom-green hover:text-white',
+              current === target
+                ? 'bg-custom-green text-white'
+                : 'bg-white text-gray-400',
+            )}
           >
             <items.Icon />
           </Button>
