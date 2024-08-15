11b02c9891c8d04b51c848b968d3093019f82050
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 14:05:07 +0800

    feat: pass card data instead.

--- apps/tapup/app/home/(user)/cards/_components/home-card.tsx
+++ apps/tapup/app/home/(user)/cards/_components/home-card.tsx
@@ -1,34 +1,28 @@
 import { PenLine } from 'lucide-react'
 import React from 'react'
-
-interface cardData {
-  id: string
-  name: string
-  role: string
-  link: string
-}
+import Card from '~/types/cards'
 
 interface CardProps {
-  cardData: cardData
+  card: Card
   className?: string
   height?: string
   width?: string
 }
 
-const HomeCard = ({ cardData, className, height, width }: CardProps) => {
-  const { id, name, role, link } = cardData
+const HomeCard = ({ card, className, height, width }: CardProps) => {
+  const { id, name, industry, status, username_url } = card
   return (
     <div
-      className={`flex h-[222px] w-[300px] flex-col rounded-[20px] bg-black px-6 pb-6 pt-8 text-white ${className}`}
+      className={`flex h-[15vw] w-[24vw] flex-col rounded-[20px] bg-black px-6 pb-6 pt-8 text-white ${className}`}
       style={{ width: width, height: height }}
     >
       <div className="flex flex-1 flex-col items-end">
         <p className="text-xs font-bold">{name}</p>
-        <p className="text-[10px]">{role}</p>
+        <p className="text-[10px]">{industry}</p>
       </div>
       <div className="flex items-center justify-between">
-        <a href={link} className="text-[8px] text-[#b4b4b4]">
-          {link}
+        <a href={username_url} className="text-[8px] text-[#b4b4b4]">
+          {username_url}
         </a>
         <a href="#" className="text-right text-[#b4b4b4]">
           <PenLine size={10} fill="#b4b4b4" />
