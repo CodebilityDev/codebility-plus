326c284a875b6781fbaaf64074536f0bb20f2851
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 13:51:56 +0800

    feat: cards type.

---
+++ apps/tapup/types/cards.ts
@@ -0,0 +1,10 @@
+export default interface Card {
+    id: string;
+    name: string;
+    username_url: string;
+    code: number;
+    status: boolean;
+    industry: string;
+    created_at: string;
+    updated_at: string;
+}
