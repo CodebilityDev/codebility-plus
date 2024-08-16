5c407ef813c6ad367558d15513c7a95a5f1709a3
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-16 17:09:49 +0800

    refactor: get card id.

--- apps/tapup/app/home/builder/[id]/layout.tsx
+++ apps/tapup/app/home/builder/[id]/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import HomeNavbar from '../(user)/_components/home-navbar'
-import { UserWorkspaceContextProvider } from '../(user)/_components/user-workspace-context'
+import HomeNavbar from '../../(user)/_components/home-navbar'
+import { UserWorkspaceContextProvider } from '../../(user)/_components/user-workspace-context'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import BuilderFormProvider from './_components/builder-form-context'
+import BuilderFormProvider from '../_components/builder-form-context'
 import appConfig from '~/config/app.config'
 import { cn } from '@codevs/ui'
+import { Card } from '../_types/cards'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode
+  params: { id: string }
 }>) {
   const supabase = createServerComponentClient({ cookies })
 
@@ -32,13 +35,28 @@ export default async function RootLayout({
     .eq('id', user?.id)
     .single()
 
+  const { data: card, error: fetchingCardError } = await supabase
+    .from('cards')
+    .select(
+      `id,
+       name,
+       username_url,
+       status,
+       industry`,
+    )
+    .eq('id', params.id)
+    .eq('user_id', user?.id)
+    .single()
+
   return (
     <html lang={appConfig.locale}>
       <body className={cn(inter.className, appConfig.theme)}>
         <UserWorkspaceContextProvider value={data}>
           <div className="fixed -z-10 h-full w-full bg-slate-100"></div>
           <HomeNavbar />
-          <BuilderFormProvider>{children}</BuilderFormProvider>
+          <BuilderFormProvider cardData={card as Card}>
+            {children}
+          </BuilderFormProvider>
         </UserWorkspaceContextProvider>
       </body>
     </html>
