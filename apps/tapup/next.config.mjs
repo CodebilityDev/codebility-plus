a5424f2284dcc1d72bdbf4721cc69a799948e428
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-16 16:55:03 +0800

    feat: include supabase bucket sources.

--- apps/tapup/next.config.mjs
+++ apps/tapup/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL.split('https://')[1],
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
