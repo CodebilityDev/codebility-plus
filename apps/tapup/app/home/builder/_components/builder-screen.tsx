0486e29aa93da7a7bf12b9c0cd612a44a5279d17
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-16 10:46:44 +0800

    feat: builder screen.

---
+++ apps/tapup/app/home/builder/_components/builder-screen.tsx
@@ -0,0 +1,9 @@
+function BuilderScreen() {
+  return (
+    <div className="flex flex-1 justify-center">
+      <div className="h-[40vw] w-[25vw] border border-black bg-white"></div>
+    </div>
+  )
+}
+
+export default BuilderScreen
