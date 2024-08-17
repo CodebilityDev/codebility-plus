b658e57c168085288a4903513b823b49aa639ee0
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-17 16:39:15 +0800

    fix: remove extra forward slash

--- apps/tapup/middleware.ts
+++ apps/tapup/middleware.ts
@@ -76,8 +76,8 @@ function getPatterns() {
         const tenant = req.headers.get("host")?.split(".")[0];
         const protocol = req.url.split("://")[0];
         const originalHost = process.env.NODE_ENV === "production"? `.${appConfig.url}`: `.localhost:3000`; // adjust the port if needed.
-        const profileData = await getUserCards(req,res,{username_url: `${protocol}://${tenant}${originalHost}/`});
-
+        const profileData = await getUserCards(req,res,{username_url: `${protocol}://${tenant}${originalHost}`});
+        console.log(`${protocol}://${tenant}${originalHost}`)
         if (!profileData || (profileData.data && profileData.data.length === 0)) {
           return NextResponse.rewrite(new URL("/",req.url));
         } 
