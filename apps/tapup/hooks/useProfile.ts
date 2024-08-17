ef1db83168e8de83e25d088c161d0139647a74b6
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-17 16:07:58 +0800

    feat: show an error if hook is use outside provider.

--- apps/tapup/hooks/useProfile.ts
+++ apps/tapup/hooks/useProfile.ts
@@ -5,6 +5,8 @@ import { ProfileContext } from "../components/profile-context";
 
 export default function useProfile() {
     const { updateProfileDatas, profileDatas } = useContext(ProfileContext);
+    
+    if (!profileDatas) throw new Error("using useProfile hook outside profile context provider!");
 
     return { updateProfileDatas, profileDatas: JSON.parse(profileDatas) };
 }
