ca9760ce410b2c23a52af15db926d0e386d92cda
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-17 16:00:20 +0800

    update: create a separate profile context.

--- apps/tapup/app/home/builder/_components/builder-form-context.tsx
+++ apps/tapup/app/home/builder/_components/builder-form-context.tsx
@@ -1,16 +1,12 @@
 'use client'
 
-import { createContext, useContext, useState } from 'react'
+import { createContext, useState } from 'react'
 import { FormTargets } from '../_lib/builder-form-sidebar-items'
-import { ProfileData } from '../_lib/builder-data-form-datas'
-import { UserWorkspaceContext } from '../../_components/user-workspace-context'
 import Card from '~/types/cards'
 
 interface BuilderForm {
   current: FormTargets
   cardData: Card
-  profileDatas: string
-  updateProfileDatas: (datas: string) => void
   updateForm: (form: FormTargets) => void
 }
 
@@ -19,27 +15,14 @@ export const BuilderFormContext = createContext<BuilderForm>({} as BuilderForm)
 export default function BuilderFormProvider({
   children,
   cardData,
-  builderProfileData,
 }: {
   children: React.ReactNode
   cardData: Card
-  builderProfileData: ProfileData
 }) {
-  const user = useContext(UserWorkspaceContext)
   const [currentForm, setCurrentForm] = useState<FormTargets>('data')
-
-  const [profileDatas, setProfileDatas] = useState(
-    JSON.stringify(builderProfileData),
-  )
 
   function updateForm(form: FormTargets) {
     setCurrentForm(form)
-  }
-
-  function updateProfileDatas(datas: string) {
-    const profileDataParsed = JSON.parse(profileDatas)
-    URL.revokeObjectURL(profileDataParsed.coverPhoto)
-    setProfileDatas(datas)
   }
 
   return (
@@ -48,8 +31,6 @@ export default function BuilderFormProvider({
         cardData,
         current: currentForm,
         updateForm,
-        profileDatas,
-        updateProfileDatas,
       }}
     >
       {children}
