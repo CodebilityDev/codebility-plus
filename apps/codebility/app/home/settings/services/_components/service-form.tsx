582e590b0e9fb9d054b7823b551733613cebf56b
nerwinalamas <nerwinalamas@gmail.com>
2024-08-21 19:16:31 +0800

    feat: service form for create and update

---
+++ apps/codebility/app/home/settings/services/_components/service-form.tsx
@@ -0,0 +1,177 @@
+"use client"
+
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import toast from "react-hot-toast";
+import H1 from "@/Components/shared/dashboard/H1";
+import { useRouter } from "next/navigation";
+import { createServices, updateService } from "../action";
+import * as Yup from "yup";
+import { service_FormValuesT } from "@/types/protectedroutes";
+import ImageUploader from "./image-uploader";
+import InputLabel from "./input-label";
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is Required"),
+  category: Yup.string().required("Category is Required"),
+  description: Yup.string().required("Description is Required"),
+  mainImage: Yup.mixed().optional(),
+  picture1: Yup.mixed().optional(),
+  picture2: Yup.mixed().optional(),
+});
+
+const ServiceForm = ({ userId, service }: { userId?: string | null, service?: service_FormValuesT }) => {
+  const router = useRouter();
+
+  const initialValues: service_FormValuesT = {
+    name: service?.name || "",
+    category: service?.category || "",
+    description: service?.description || "",
+    mainImage: service?.mainImage || null,
+    picture1: service?.picture1 || null,
+    picture2: service?.picture2 || null,
+  };
+
+  const handleSubmit = async (values: any, actions: any) => {
+    if (!userId) return;
+
+    try {
+      const formData = new FormData();
+      formData.append("name", values.name);
+      formData.append("category", values.category);
+      formData.append("description", values.description);
+      if (values.mainImage) formData.append("mainImage", values.mainImage);
+      if (values.picture1) formData.append("picture1", values.picture1);
+      if (values.picture2) formData.append("picture2", values.picture2);
+      formData.append("userId", userId);
+
+      let response;
+
+      if (service?.id) {
+        formData.append("id", service.id);
+        response = await updateService(formData);
+      } else {
+        response = await createServices(formData);
+      }
+
+      if (response?.success) {
+        toast.success(service?.id ? "Service Updated Successfully" : "Service Created Successfully");
+        router.back();
+      } else {
+        console.error("Failed to save service:", response?.error);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("Error");
+    } finally {
+      actions.setSubmitting(false);
+    }
+  };
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+      {({ isSubmitting }) => (
+        <Form className="max-w-screen-xl mx-auto flex w-full flex-col gap-16">
+          <div className="flex justify-between">
+            <H1>{service?.id ? "Update Service" : "Add New Service"}</H1>
+            <div className="hidden lg:flex gap-4">
+              <button
+                type="button"
+                className="rounded-[5px] bg-white border dark:bg-[#2C303A] px-10 py-2 text-lg text-black dark:text-white"
+                onClick={() => router.back()}
+              >
+                Cancel
+              </button>
+              <button 
+                type="submit" 
+                className="rounded-[5px] bg-violet px-10 py-2 text-lg text-white" 
+                disabled={isSubmitting}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+          <div className="flex w-full flex-col justify-between gap-8 sm:flex-row">
+            <div className="flex flex-1 flex-col gap-4">
+              <div className="flex flex-col text-dark-300 dark:text-white">
+                <InputLabel htmlFor="name" required>
+                  Name
+                </InputLabel>
+                <Field
+                  placeholder="Write the name of the app"
+                  name="name"
+                  type="text"
+                  className="h-9 rounded-[5px] border border-[#3F3F46] px-1 text-dark-100 dark:bg-dark-100 dark:text-white"
+                />
+                <ErrorMessage name="name" component="div" className="text-[#FF4242]" />
+              </div>
+              <div className="flex flex-col text-dark-100 dark:text-white">
+                <InputLabel htmlFor="category" required>
+                  Category
+                </InputLabel>
+                <Field
+                  name="category"
+                  as="select"
+                  className="h-9 rounded-[5px] border border-[#3F3F46] text-dark-100 dark:bg-dark-100 dark:text-white"
+                >
+                  <option value="">Select a category</option>
+                  <option value="Web Application">Web Application</option>
+                  <option value="Mobile Application">Mobile Application</option>
+                  <option value="Product Design">Product Design</option>
+                </Field>
+                <ErrorMessage name="category" component="div" className="text-[#FF4242]" />
+              </div>
+              <div className="flex flex-col text-dark-300 dark:text-white">
+                <InputLabel htmlFor="description" required>
+                  Description
+                </InputLabel>
+                <Field
+                  name="description"
+                  as="textarea"
+                  placeholder="Write the description"
+                  className="h-[150px] resize-none rounded-[5px] border border-[#3F3F46] px-1 pt-1 text-dark-100 dark:bg-dark-100 dark:text-white"
+                />
+                <ErrorMessage name="description" component="div" className="text-[#FF4242]" />
+              </div>
+            </div>
+            <div className="flex flex-1 flex-col gap-4 text-black-100 dark:text-white">
+              <div>
+                <InputLabel htmlFor="mainImage">Main Image</InputLabel>
+                <ImageUploader name="mainImage" />
+                <ErrorMessage name="mainImage" component="div" className="text-[#FF4242]" />
+              </div>
+              <div>
+                <InputLabel htmlFor="picture1">Picture 1</InputLabel>
+                <ImageUploader name="picture1" />
+                <ErrorMessage name="picture1" component="div" className="text-[#FF4242]" />
+              </div>
+              <div>
+                <InputLabel htmlFor="picture2">Picture 2</InputLabel>
+                <ImageUploader name="picture2" />
+                <ErrorMessage name="picture2" component="div" className="text-[#FF4242]" />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-center lg:hidden">
+            <button
+              type="button"
+              className="rounded-[5px] bg-white border dark:bg-[#2C303A] px-10 py-2 text-lg text-black dark:text-white"
+              onClick={() => router.back()}
+            >
+              Cancel
+            </button>
+            <button 
+              type="submit" 
+              className="rounded-[5px] bg-violet px-10 py-2 text-lg text-white" 
+              disabled={isSubmitting}
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ServiceForm;
