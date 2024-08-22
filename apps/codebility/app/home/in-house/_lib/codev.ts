53cd7d238e4ae2a03d352011c5985b8bdfda8267
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-22 17:18:17 +0800

    feat: added codev property.

--- apps/codebility/app/home/in-house/_lib/codev.ts
+++ apps/codebility/app/home/in-house/_lib/codev.ts
@@ -17,9 +17,13 @@ export interface Codev {
     id: string;
     first_name: string;
     last_name: string;
+    image_url: string;
+    address: string;
+    about_me: string;
     main_position: string;
     internal_status: InternalStatus;
     projects: Project[];
     tech_stacks: string[];
     nda_status: string;
+    job_status: string;
 }
