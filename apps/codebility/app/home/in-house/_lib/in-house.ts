699976ec8b799212f4f5a73d1173457640be37fa
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-22 15:45:52 +0800

    feat: in house types.

---
+++ apps/codebility/app/home/in-house/_lib/in-house.ts
@@ -0,0 +1,26 @@
+import { Codev } from "./codev"
+
+export interface InHouseProps {
+    data: Codev[]
+    editableIds: string[]
+    handlers: {
+      setData: React.Dispatch<React.SetStateAction<Codev[]>>
+      handleEditButton: (id: string) => void
+      handleSaveButton: (updatedMember: Codev) => void
+    }
+    status: {
+      LoadinginHouse: boolean
+      ErrorinHouse: Error | null
+    }
+    currentPage: number
+    totalPages: number
+    handleNextPage: () => void
+    handlePreviousPage: () => void
+}
+
+export interface InHouseEditable {
+  data: Codev;
+  handleSaveButton: InHouseProps["handlers"]["handleSaveButton"];
+}
+
+export type InHouseSort = (key: keyof Codev, direction: "up" | "down") => void;
