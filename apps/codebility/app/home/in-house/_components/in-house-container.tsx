8fc82611695c7f0741718732fb222401718825bf
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-22 15:46:58 +0800

    feat: create a container for client components of inhouse.

---
+++ apps/codebility/app/home/in-house/_components/in-house-container.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import Table from "./in-house-table"
+import InHouseCards from "./in-house-cards"
+import { TeamMemberT } from "@/types/index"
+import usePagination from "@/hooks/use-pagination"
+import { Codev } from "../_lib/codev";
+
+interface Props {
+  codevData: Codev[];
+}   
+
+export default function InHouseContainer({ codevData }: Props) {
+  const [data, setData] = useState<Codev[]>(codevData);
+    const [editableIds, setEditableIds] = useState<string[]>([]);
+
+    const handleEditButton = (id: string) => {
+        setEditableIds((prevIndexes) => [...prevIndexes, id])
+    }
+
+  const handleSaveButton = (updatedMember: Codev) => {
+    setEditableIds((prevIndexes) => prevIndexes.filter((editableId) => editableId !== updatedMember.id))
+  }
+
+  const LoadinginHouse = false;
+  const ErrorinHouse = null;
+
+  const { currentPage, totalPages, paginatedData, handlePreviousPage, handleNextPage } = usePagination(codevData, 10)
+
+    return <div>
+        <Table
+        data={paginatedData}
+        editableIds={editableIds}
+        handlers={{
+          setData,
+          handleEditButton,
+          handleSaveButton,
+        }}
+        status={{
+          LoadinginHouse,
+          ErrorinHouse,
+        }}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+      />
+      <InHouseCards
+        data={paginatedData}
+        editableIds={editableIds}
+        handlers={{
+          setData,
+          handleEditButton,
+          handleSaveButton,
+        }}
+        status={{
+          LoadinginHouse,
+          ErrorinHouse,
+        }}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+      />
+    </div>
+}
