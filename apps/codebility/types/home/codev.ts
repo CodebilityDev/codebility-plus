25539991f89c10bc62c6fe56937a23efbee7b19b
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-24 12:46:49 +0800

    feat: re move type to home folder.

---
+++ apps/codebility/types/home/codev.ts
@@ -0,0 +1,31 @@
+export interface Codev {
+    id: string;
+    user_id: string;
+    first_name: string;
+    last_name: string;
+    image_url: string;
+    address: string;
+    about: string;
+    socials: Record<string, string | null>;
+    main_position: string;
+    internal_status: InternalStatus;
+    projects: Project[];
+    tech_stacks: string[];
+    nda_status: string;
+    job_status: string;
+  }
+  
+export interface Project {
+    id: string;
+    name: string;
+  }
+  
+export type InternalStatus = 
+      | "AVAILABLE"
+      | "DEPLOYED"
+      | "TRAINING"
+      | "VACATION"
+      | "BUSY"
+      | "CLIENTREADY"
+      | "GRADUATED"
+      | "FAILED";
