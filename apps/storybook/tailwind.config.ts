373ff102bf56d381917695ecdc38a42b149a9189
Nemuel M Gabitanan Jr <nemuelmgabitananjr@gmail.com>
2024-08-25 20:42:36 +0800

    feature: added storybook global test components

--- apps/storybook/tailwind.config.ts
+++ apps/storybook/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import tailwindCssAnimate from "tailwindcss-animate"
 
 const config = {
   darkMode: ["class"],
@@ -74,7 +75,8 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  // plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindCssAnimate],
 } satisfies Config
 
 export default config
