80866e9fd7eba90c6b0e1b2e66dbb98f789a7ba0
Nemuel M Gabitanan Jr <nemuelmgabitananjr@gmail.com>
2024-08-25 23:08:20 +0800

    feat: added the Add New Board button shadcn sample button

--- apps/codebility/stories/Button.stories.tsx
+++ apps/codebility/stories/Button.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from 'storybook/test'
 import { action } from '@storybook/addon-actions'
 import { Button } from '@codevs/ui/button'
+// import { IoAddCircle } from 'react-icons/io5'
 
 const meta = {
   title: 'ShadCN-Atomic/Button',
@@ -38,12 +39,34 @@ const meta = {
       control: 'select',
       description: 'Button color variants',
       options: ['default', 'primary', 'secondary', 'destructive', 'success'],
-    }
+    },
   },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const StartTimer: Story = {
+	args: {
+    variant: 'primary',
+		size: 'lg',
+		disabled: false,
+		onClick: action('StartTimer click'),
+		children: 'Start Timer',
+		className: "inline-flex w-full items-center justify-center duration-300 px-6 py-2 whitespace-nowrap rounded-md text-md lg:text-lg ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-100 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-blue-100 text-white hover:bg-blue-200 h-10"
+	}
+};
+
+export const AddNewBoard: Story = {
+	args: {
+    variant: 'primary',
+		size: 'lg',
+		disabled: false,
+		onClick: action('AddNewBoard click'),
+		children: 'Add New Board',
+		className: "justify-center duration-300 px-6 py-2 whitespace-nowrap rounded-md text-md lg:text-lg ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-100 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-blue-100 text-white hover:bg-blue-200 h-10 flex w-max items-center gap-2"
+	},
+};
 
 export const Default: Story = {
   args: {
@@ -52,7 +75,7 @@ export const Default: Story = {
     disabled: false,
     onClick: action('default click'),
     children: 'Default Button',
-    className: 'shadow-lg bg-green-600 text-white'
+    className: "shadow-lg"
   },
 };
 
@@ -78,13 +101,3 @@ export const Secondary: Story = {
 	}
 };
 
-export const StartTimer: Story = {
-	args: {
-    variant: 'secondary',
-		size: 'lg',
-		disabled: false,
-		onClick: action('secondary click'),
-		children: 'Start Timer',
-		className: 'bg-blue-100 hover:bg-blue-200 text-white'
-	}
-};
