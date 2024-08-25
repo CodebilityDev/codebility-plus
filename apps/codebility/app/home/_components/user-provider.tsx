1a89e34a03bf7e691f4c3a692446f36ad29212ab
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-25 20:59:54 +0800

    feat: user context.

---
+++ apps/codebility/app/home/_components/user-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { createContext } from "react";
+
+export interface User {
+    first_name: string;
+    last_name: string;
+    email: string;
+    main_position: string;
+    permissions: string[];
+}
+
+export const UserContext = createContext<User>({} as User);
+
+export default function UserContextProvider({
+    children,
+    userData
+}: {
+    children: React.ReactNode,
+    userData: User
+}) {
+
+    return <UserContext.Provider value={userData}>
+        {children}
+    </UserContext.Provider>
+}
