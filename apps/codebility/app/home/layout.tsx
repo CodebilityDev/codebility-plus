81d8b3d29d5e34e359d332b0ebcdf69eaac239a8
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-25 21:00:00 +0800

    feat: get user info.

--- apps/codebility/app/home/layout.tsx
+++ apps/codebility/app/home/layout.tsx
@@ -3,19 +3,62 @@ import LeftSidebar from "@/Components/shared/dashboard/LeftSidebar"
 import Navbar from "@/Components/shared/dashboard/Navbar"
 import React from "react"
 import ReactQueryProvider from "@/hooks/reactQuery"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import UserContextProvider from "./_components/user-provider"
 
-export default function Template({ children }: { children: React.ReactNode }) {
+export default async function HomeLayout({ children }: { children: React.ReactNode }) {
+  const supabase = createServerComponentClient( { cookies } );
+
+  const { data: { user } } = await supabase.auth.getUser();
+
+  const { data } = await supabase.from("user")
+  .select(`
+    *,
+    user_type(
+      roles,
+      kanban,
+      clients,
+      interns,
+      my_task,
+      in_house,
+      projects,
+      services,
+      dashboard,
+      applicants,
+      org_charts,
+      permissions,
+      time_tracker
+    ),
+    profile(*)
+  `).eq('id', user?.id)
+  .single();
+
+  const permissionNames = Object.keys(data?.user_type || {});
+  const permissions = permissionNames.filter(permissionName => data.user_type[permissionName]);
+  const { first_name, last_name, main_position } = data.profile;
+
+  const userData = {
+    first_name,
+    last_name,
+    email: data.email,
+    main_position,
+    permissions
+  };
+
   return (
     <ReactQueryProvider>
-      <main className="background-light850_dark100 relative">
-        <Navbar />
-        <div className="flex">
-          <LeftSidebar />
-          <section className="background-lightsection_darksection flex min-h-screen flex-1 flex-col px-4 pb-6 pt-36 max-md:pb-14 md:px-8 lg:px-12">
-            <div className="max-w-8xl mx-auto h-full w-full">{children}</div>
-          </section>
-        </div>
-      </main>
+      <UserContextProvider userData={userData}>   
+        <main className="background-light850_dark100 relative">
+          <Navbar />
+          <div className="flex">
+            <LeftSidebar />
+            <section className="background-lightsection_darksection flex min-h-screen flex-1 flex-col px-4 pb-6 pt-36 max-md:pb-14 md:px-8 lg:px-12">
+              <div className="max-w-8xl mx-auto h-full w-full">{children}</div>
+            </section>
+          </div>
+        </main>
+      </UserContextProvider>
     </ReactQueryProvider>
   )
 }
