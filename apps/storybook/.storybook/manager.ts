cc7038a398a62bc1b51fdd0faff3813382577d82
Nemuel M Gabitanan Jr <nemuelmgabitananjr@gmail.com>
2024-08-27 03:02:56 +0800

    feat: updated the storybook global interface

---
+++ apps/storybook/.storybook/manager.ts
@@ -0,0 +1,21 @@
+import { addons } from '@storybook/manager-api'
+import { create } from '@storybook/theming'
+
+addons.setConfig({
+  theme: create({
+    base: 'dark',
+
+    //Typography
+    fontBase: '"Open Sans", sans-serif',
+    fontCode: 'monospace',
+
+    //Logo section
+    brandTitle: 'Codebility',
+    brandUrl: 'https://staging.codebility.tech/',
+    brandImage: '/assets/images/codebility.png',
+    brandTarget: '_blank',
+
+    colorPrimary: '#000000',
+    colorSecondary: '#9747FF',
+  })
+})
