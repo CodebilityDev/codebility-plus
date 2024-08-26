7d2098871b5524298f1702edc7bfe7af5d3b87b8
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-27 00:02:33 +0800

    update: removed caching in server component client.

--- packages/supabase/src/clients/server-component.client.ts
+++ packages/supabase/src/clients/server-component.client.ts
@@ -2,11 +2,14 @@ import 'server-only';
 
 import { cookies } from "next/headers";
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { unstable_noStore as noStore } from 'next/cache';
 
 /**
  * @name getSupabaseServerComponentClient
  * @description Get a Supabase client for use in the Server Components
  */
 export function getSupabaseServerComponentClient() {
+    // prevent any caching (to be removed in Next v15)
+    noStore();
     return createServerComponentClient({ cookies });
 }
