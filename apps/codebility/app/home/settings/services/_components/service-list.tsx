451ff06613d1defefc804764bb0997cdb470c472
nerwinalamas <nerwinalamas@gmail.com>
2024-08-26 19:23:34 +0800

    styles: service table

--- apps/codebility/app/home/settings/services/_components/service-list.tsx
+++ apps/codebility/app/home/settings/services/_components/service-list.tsx
@@ -27,25 +27,31 @@ const ServiceList = ({ data }: any) => {
 
     return (
         <TableBody className="grid gap-5 md:grid-cols-2 lg:grid-cols-1 lg:gap-0">
-            {data?.map((service: any) => (
-                <TableRow key={service.id} className="grid background-box lg:grid-cols-8">
-                    <TableCell className="p-4 text-xl font-light lg:col-span-2">{service.name}</TableCell>
-                    <TableCell className="p-4 text-xl font-light lg:col-span-2">{service.category}</TableCell>
-                    <TableCell className="p-4 text-xl font-light truncate lg:col-span-2">{service.description}</TableCell>
-                    <TableCell className="p-4 text-xl font-light">
-                        <IconEditFillNone
-                            onClick={() => router.push(`/home/settings/services/${service.id}`)}
-                            className="lg:mx-auto cursor-pointer text-black dark:text-white duration-100 hover:scale-105" 
-                        />
-                    </TableCell>
-                    <TableCell className="p-4 text-xl font-light">
-                        <IconDelete 
-                            onClick={() => handleDelete(service.id)} 
-                            className="lg:mx-auto cursor-pointer text-blue-100 duration-100 hover:scale-105" 
-                        />
-                    </TableCell>
+            {data.length > 0 ? (
+                data.map((service: any) => (
+                    <TableRow key={service.id} className="grid background-box lg:grid-cols-8">
+                        <TableCell className="p-4 text-xl font-light lg:col-span-2">{service.name}</TableCell>
+                        <TableCell className="p-4 text-xl font-light lg:col-span-2">{service.category}</TableCell>
+                        <TableCell className="p-4 text-xl font-light truncate lg:col-span-2">{service.description}</TableCell>
+                        <TableCell className="p-4 text-xl font-light">
+                            <IconEditFillNone
+                                onClick={() => router.push(`/home/settings/services/${service.id}`)}
+                                className="lg:mx-auto cursor-pointer text-black dark:text-white duration-100 hover:scale-105" 
+                            />
+                        </TableCell>
+                        <TableCell className="p-4 text-xl font-light">
+                            <IconDelete 
+                                onClick={() => handleDelete(service.id)} 
+                                className="lg:mx-auto cursor-pointer text-blue-100 duration-100 hover:scale-105" 
+                            />
+                        </TableCell>
+                    </TableRow>
+                ))
+            ) : (
+                <TableRow>
+                    <TableCell colSpan={5} className="p-4 text-center text-gray-500 dark:text-gray-400">No data available</TableCell>
                 </TableRow>
-            ))}
+            )}
         </TableBody>
   )
 }
