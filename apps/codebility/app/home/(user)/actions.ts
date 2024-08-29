d07f386764181f77c14f976fe4d768658e43f9ae
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-30 04:46:33 +0800

    feat: log user time for task.

--- apps/codebility/app/home/(user)/actions.ts
+++ apps/codebility/app/home/(user)/actions.ts
@@ -28,7 +28,45 @@ export const startUserTimer = async (codevId: string) => {
     const { error } = await supabase.from("codev")
     .update({
         task_timer_start_at: currentDate
-    }).eq("id", codevId);
+    }).eq("id", codevId)
+    .select();
 
-    console.log(error);
+    console.log("start user timer error ",error);
+}
+
+export const logUserTime = async (formData: FormData) => {
+    const supabase = getSupabaseServerActionClient();
+
+    const codevId = formData.get("codevId");
+    const taskId = formData.get("taskId");
+
+    const { data: codevData, error: fetchingCodevError } = await supabase.from("codev")
+    .select("*")
+    .eq("id", codevId)
+    .single();
+
+    if (fetchingCodevError) throw fetchingCodevError;
+
+    const taskDuration = codevData.task && codevData.task.duration;
+    const startTime = new Date(codevData?.task_timer_start_at);
+    const endTime = new Date();
+    const workedHours = (endTime.getTime() - startTime.getTime()) / 1000 / 60 / 60;
+    const excessHours = taskDuration && workedHours - taskDuration
+
+    const { error } = await supabase.from("time_log")
+    .insert({
+        codev_id: codevId,
+        task_id: taskId,
+        time_start: startTime,
+        time_end: endTime,
+        worked_hours: workedHours,
+        excess_hours: (excessHours && excessHours > 0) ? excessHours : 0
+    })
+
+    if (error) throw error;
+
+    await supabase.from("codev")
+    .update({
+        task_timer_start_at: null
+    }).eq("id", codevId);
 }
