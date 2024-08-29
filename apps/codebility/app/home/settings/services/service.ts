0b10646623ccd023344b933db2970acf48a1ca44
nerwinalamas <nerwinalamas@gmail.com>
2024-08-29 12:23:01 +0800

    refactor: get service image

--- apps/codebility/app/home/settings/services/service.ts
+++ apps/codebility/app/home/settings/services/service.ts
@@ -18,6 +18,7 @@ export const getAllServices = async () => {
 
 export const getServiceById = async (serviceId: string) => {
     const supabase = await createServer();
+
     const { data, error } = await supabase
         .from("services")
         .select("*")
@@ -29,5 +30,51 @@ export const getServiceById = async (serviceId: string) => {
         return { success: false, error: error.message };
     }
 
-    return { success: true, data };
+    const getFileDetails = async (filePath: string) => {
+        const fileName = filePath.split('/').pop();
+
+        const { data: fileMetadata, error: fileError } = await supabase
+            .storage
+            .from("services-image")
+            .list("public"); 
+
+        if (fileError || !fileMetadata || fileMetadata.length === 0) {
+            console.log("Error fetching file details: ", fileError?.message);
+            return null;
+        }
+
+        const file = fileMetadata.find(file => file.name === fileName);
+        if (!file) {
+            console.log(`File not found: ${fileName}`);
+            return null;
+        }
+
+        const { data: publicUrl } = supabase
+            .storage
+            .from("services-image")
+            .getPublicUrl(filePath);
+
+        return {
+            fileName: file.name,
+            metadata: file.metadata || {},
+            createdAt: file.created_at,
+            url: publicUrl.publicUrl,
+        };
+    };
+
+    const images = [];
+    if (data?.mainImage) {
+        const mainImageDetails = await getFileDetails(data.mainImage);
+        if (mainImageDetails) images.push({ mainImage: mainImageDetails });
+    }
+    if (data?.picture1) {
+        const picture1Details = await getFileDetails(data.picture1);
+        if (picture1Details) images.push({ picture1: picture1Details });
+    }
+    if (data?.picture2) {
+        const picture2Details = await getFileDetails(data.picture2);
+        if (picture2Details) images.push({ picture2: picture2Details });
+    }
+
+    return { success: true, data: { ...data, images } };
 }
