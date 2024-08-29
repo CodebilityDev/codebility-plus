a9c638e70fdb773efb21863be071d2fc58def49e
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-29 23:09:56 +0800

    update: make component a server component.

--- apps/codebility/app/home/(user)/_components/dashboard-time-tracker.tsx
+++ apps/codebility/app/home/(user)/_components/dashboard-time-tracker.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { useState } from "react"
 import { Box } from "@/Components/shared/dashboard"
 import { Skeleton } from "@/Components/ui/skeleton/skeleton"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/Components/ui/select"
-import { useQuery } from "@tanstack/react-query"
-import { getSupabaseBrowserClient } from "@codevs/supabase/browser-client";
-import useUser from "../../_hooks/use-user";
 import TimeTrackerTimer from "./dashboard-time-tracker-timer";
 import TimeTrackerSchedule from "./dashboard-time-tracker-schedule";
+import { getSupabaseServerComponentClient } from "@codevs/supabase/server-component-client"
 
 interface Task {
   title: string;
@@ -16,54 +11,56 @@ interface Task {
   points: number;
 }
 
-export default function TimeTracker() {
-  const user = useUser();
-  const [selectedTask, setSelectedTask]: any = useState<Task | null>(null)
+export default async function TimeTracker() {
+  const supabase = getSupabaseServerComponentClient();
+  const { data: { user }} = await supabase.auth.getUser();
 
-  const { data: TrackerTask, isLoading: tasksLoading } = useQuery<Task[]>({
-    queryKey: ["Tasks", "Kanban"],
-    queryFn: async () => {
-      const supabase = getSupabaseBrowserClient();
-      const { data, error} = await supabase.from("codev_task")
-      .select(
-        `
-          *,
-          task(*)
-        `
-      )
-      .eq("codev_id", user.codev_id);
+  const { data, error } = await supabase.from("user")
+  .select(`
+    *,
+    codev(
+      id,
+      codev_task(
+        task(
+          title,
+          duration,
+          points
+        )
+      ),
+      start_time,
+      end_time
+    )
+  `).eq("id", user?.id)
+  .single();
 
-      if (error) throw error;
-      
-      const tasks = data.map(item => item.task);
-      return tasks;
-    },
-    refetchInterval: 3000,
-  })
+  if (error) return <Box className="flex-1">
+    <div className="mx-auto flex flex-col items-center gap-3">
+      <Skeleton className="h-9 w-full" />
+      <Skeleton className="h-8 w-full" />
+      <Skeleton className="h-8 w-full" />
+      <Skeleton className="h-48 w-full" />
+    </div>
+  </Box>
+
+  const tasks = data.codev.codev_task.map((item: { task: Task }) => item.task);
 
   return (
     <>
-      { !tasksLoading ? (
-        <Box className="flex w-full flex-1 flex-col items-center gap-4">
+      <Box className="flex w-full flex-1 flex-col items-center gap-4">
           <div>
             <p className="text-2xl">Time Tracker</p>
           </div>
 
           <div className="w-full">
             <p className="text-md text-center text-gray">My Time Schedule</p>
-            <TimeTrackerSchedule codevId={user.codev_id} startTime={user.start_time} endTime={user.end_time} />
+            <TimeTrackerSchedule codevId={data.codev.id} startTime={data.codev.start_time} endTime={data.codev.end_time} />
           </div>
           <div className="flex w-full flex-col items-center gap-6 rounded-lg border border-zinc-200 p-4 dark:border-zinc-700">
-            <Select
-              onValueChange={(value) => {
-                const task = TrackerTask?.find((t) => t.title === value)
-                setSelectedTask(task)
-              }}
-            >
+            <Select>
               <SelectTrigger className="max-w-[300px] text-center">
                 <SelectValue placeholder="Select Task" />
                 <SelectContent>
-                  {TrackerTask?.map((task: Task, index:number) =>
+                  {tasks.map((task: Task, index:number) =>
                     <SelectItem className="items-center" key={index} value={task.title}>
                         {task.title} - {task.duration && `${task.duration}h - `} {task.points}pts
                     </SelectItem>
@@ -73,17 +70,7 @@ export default function TimeTracker() {
             </Select>
             <TimeTrackerTimer />
           </div>
-        </Box>
-      ) : (
-        <Box className="flex-1">
-          <div className="mx-auto flex flex-col items-center gap-3">
-            <Skeleton className="h-9 w-full" />
-            <Skeleton className="h-8 w-full" />
-            <Skeleton className="h-8 w-full" />
-            <Skeleton className="h-48 w-full" />
-          </div>
-        </Box>
-      )}
+      </Box>
     </>
   )
 }
