27e706f2c45498b7483b53c24203455953e046de
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-30 14:41:00 +0800

    feat: display time log in mobile.

--- apps/codebility/app/home/time-tracker/_components/time-tracker-table-mobile.tsx
+++ apps/codebility/app/home/time-tracker/_components/time-tracker-table-mobile.tsx
@@ -1,30 +1,35 @@
 import { logData } from "@/app/home/time-tracker/_lib/dummy-data"
 import { IconEdit } from "@/public/assets/svgs"
 import { Table, TableRow, TableCell, TableBody } from "@/Components/ui/table"
+import { TimeLog } from "../_types/time-log";
 
-const TimeTrackerTableMobile = () => {
+interface Props {
+  timeLog: TimeLog[];
+}
+
+const TimeTrackerTableMobile = ({ timeLog }: Props) => {
   return (
     <>
-      {logData.map((value, index) => (
+      {timeLog.map((log, index) => (
         <Table
           key={index}
           className="background-box text-dark100_light900 my-[10px] flex h-[355px] flex-col  rounded border border-zinc-200 shadow-sm dark:border-zinc-700 "
         >
           <TableBody className="flex flex-col">
             <TableRow>
-              <TableCell>Task: {value.task}</TableCell>
+              <TableCell>Task: {log.task.title}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Points: {value.points}</TableCell>
+              <TableCell>Points: {0}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Rendered Hours: {value.renderedHours}</TableCell>
+              <TableCell>Rendered Hours: {log.worked_hours}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Duration Hours: {value.durationHours}</TableCell>
+              <TableCell>Duration Hours: {log.task.duration}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Project: {value.project}</TableCell>
+              <TableCell>Project: {log.task.project.name}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell className="flex cursor-pointer justify-end">
