407185944b83cfe88a5be67637f8aa5ca698d38e
josephencila <joseph.mesia.encila@gmail.com>
2024-08-30 17:07:43 +0800

    fetch applicants from page.tsx

--- apps/codebility/app/home/applicants/page.tsx
+++ apps/codebility/app/home/applicants/page.tsx
@@ -1,4 +1,11 @@
-const ApplicantsPage = () => {
+import { getSupabaseServerComponentClient } from "@codevs/supabase/server-component-client"
+const ApplicantsPage = async () => {
+
+  const supabase = getSupabaseServerComponentClient();
+  const { data: applicants, error } = await supabase
+    .from("applicants")
+    .select('*')
+
 
   return (
     <div className="max-w-screen-xl mx-auto flex flex-col gap-4">
