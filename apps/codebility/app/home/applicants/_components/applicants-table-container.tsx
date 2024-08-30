23d5bdd49c8cc6d7aba2ad71bd9ed76d863ef594
josephencila <joseph.mesia.encila@gmail.com>
2024-08-30 17:40:14 +0800

    feat: added pagination to tables

--- apps/codebility/app/home/applicants/_components/applicants-table-container.tsx
+++ apps/codebility/app/home/applicants/_components/applicants-table-container.tsx
@@ -4,14 +4,38 @@ import ApplicantsTableDesktop from "@/app/home/applicants/_components/applicants
 import ApplicantsTableMobile from "@/app/home/applicants/_components/applicants-table-mobile"
 import { Suspense } from 'react'
 import ApplicantsLoading from "@/app/home/applicants/loading"
+import usePagination from '@/hooks/use-pagination'
+import { pageSize } from '@/constants'
+import DefaultPagination from '@/Components/ui/pagination'
+
 const ApplicantsTableContainer = ({ applicants }: { applicants: ApplicantsList_Types[] }) => {
+    const {
+        currentPage,
+        totalPages,
+        paginatedData: paginatedApplicants,
+        handlePreviousPage,
+        handleNextPage,
+        setCurrentPage,
+    } = usePagination(applicants || [], pageSize.applicants)
+
+
     return (
         <>
             <Suspense fallback={<ApplicantsLoading />}>
-                <ApplicantsTableDesktop applicants={applicants} />
-                <ApplicantsTableMobile applicants={applicants} />
+                <ApplicantsTableDesktop applicants={paginatedApplicants} />
+                <ApplicantsTableMobile applicants={paginatedApplicants} />
             </Suspense>
-
+            <div className="relative w-full">
+                {applicants.length > pageSize.applicants && (
+                    <DefaultPagination
+                        currentPage={currentPage}
+                        handleNextPage={handleNextPage}
+                        handlePreviousPage={handlePreviousPage}
+                        setCurrentPage={setCurrentPage}
+                        totalPages={totalPages}
+                    />
+                )}
+            </div>
         </>
     )
 }
