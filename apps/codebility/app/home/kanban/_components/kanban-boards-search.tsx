cb576fa00dcf0512e97655d65cc78a511febc13f
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-31 06:11:39 +0800

    feat: search for filtering data.

--- apps/codebility/app/home/kanban/_components/kanban-boards-search.tsx
+++ apps/codebility/app/home/kanban/_components/kanban-boards-search.tsx
@@ -1,14 +1,26 @@
 "use client"
 
-import { useState } from "react"
+import { usePathname, useSearchParams, useRouter } from "next/navigation"
 
 export default function KanbanBoardsSearch() {
-  const [isSearching, setIsSearching] = useState("")
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (term) params.set("query", term)
+    else params.delete("query");
+    replace(`${pathname}?${params.toString()}`);
+  }
 
   return (
     <input
       type="text"
       placeholder="Search board"
+      onChange={(e) => handleSearch(e.target.value)}
+      defaultValue={searchParams.get("query")?.toString()}
       className="h-10 w-full rounded-full border border-gray border-opacity-50 bg-inherit px-5 text-xs focus:outline-none md:w-80"
     />
   )
