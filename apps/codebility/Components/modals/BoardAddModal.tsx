983a63b016a5c233ad2f4b3744c28eb791c97cdc
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-30 15:34:33 +0800

    style: make text visible.

--- apps/codebility/Components/modals/BoardAddModal.tsx
+++ apps/codebility/Components/modals/BoardAddModal.tsx
@@ -85,7 +85,7 @@ const BoardAddModal = () => {
   return (
     <Dialog open={isModalOpen}>
       <DialogContent
-        className="flex h-auto w-[100%] max-w-3xl flex-col gap-6 overflow-x-auto overflow-y-auto"
+        className="background-lightbox_darkbox text-dark100_light900 flex h-auto w-[100%] max-w-3xl flex-col gap-6 overflow-x-auto overflow-y-auto"
       >
         <DialogHeader className="relative">
           <DialogTitle className="mb-2 text-left text-lg">Add New Board</DialogTitle>
