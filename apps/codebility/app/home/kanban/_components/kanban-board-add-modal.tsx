e53df831781c4c33c19a5cfd537dc20b43d0a71e
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-31 05:18:28 +0800

    feat: create new board function.

--- apps/codebility/app/home/kanban/_components/kanban-board-add-modal.tsx
+++ apps/codebility/app/home/kanban/_components/kanban-board-add-modal.tsx
@@ -16,6 +16,7 @@ import { DialogTrigger } from "@radix-ui/react-dialog"
 import { getSupabaseServerComponentClient } from "@codevs/supabase/server-component-client"
 import type { Project } from "@/types/home/codev"
 import Input from "@/Components/ui/forms/input"
+import { createNewBoard } from "../actions"
 
 export default async function KanbanBoardAddModal() {
   const supabase = getSupabaseServerComponentClient();
@@ -38,56 +39,58 @@ export default async function KanbanBoardAddModal() {
         className="background-lightbox_darkbox text-dark100_light900 flex h-auto w-[100%] max-w-3xl flex-col gap-6 overflow-x-auto overflow-y-auto"
         hideCloseButton={false}
       >
-        <DialogHeader className="relative">
-          <DialogTitle className="mb-2 text-left text-lg">Add New Board</DialogTitle>
-        </DialogHeader>
+        <form action={createNewBoard}>
+          <DialogHeader className="relative">
+            <DialogTitle className="mb-2 text-left text-lg">Add New Board</DialogTitle>
+          </DialogHeader>
 
-        <div className="flex flex-col gap-6 lg:flex-row">
-          <div className="flex basis-[50%] flex-col gap-4">
-            <div>
-              <Label>Project</Label>
-              <Select>
-                <SelectTrigger
-                  aria-label="Projects"
-                  className="border-light_dark flex w-full items-center justify-between rounded border bg-transparent px-3 py-2 text-left text-sm focus:outline-none"
-                >
-                  <SelectValue className="text-sm" placeholder="Select a Project" />
-                  <IconDropdown className="invert dark:invert-0" />
-                </SelectTrigger>
+          <div className="flex flex-col gap-6 lg:flex-row">
+            <div className="flex basis-[50%] flex-col gap-4">
+              <div>
+                <Label>Project</Label>
+                <Select name="projectId">
+                  <SelectTrigger
+                    aria-label="Projects"
+                    className="border-light_dark flex w-full items-center justify-between rounded border bg-transparent px-3 py-2 text-left text-sm focus:outline-none"
+                  >
+                    <SelectValue className="text-sm" placeholder="Select a Project" />
+                    <IconDropdown className="invert dark:invert-0" />
+                  </SelectTrigger>
 
-                <SelectContent className="border-light_dark rounded-md border bg-[#FFF] dark:bg-black-100">
-                  <SelectGroup>
-                    <SelectLabel className="px-3 py-2 text-xs text-gray">Projects</SelectLabel>
-                    {projects?.map(({ id, name }: Project) => (
-                      <SelectItem
-                        key={id}
-                        className="w-[345px] cursor-default px-3 py-2 text-sm hover:bg-blue-100"
-                        value={id}
-                      >
-                        <SelectItemText>{name}</SelectItemText>
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div>
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" type="text" name="name"/>
+                  <SelectContent className="border-light_dark rounded-md border bg-[#FFF] dark:bg-black-100">
+                    <SelectGroup>
+                      <SelectLabel className="px-3 py-2 text-xs text-gray">Projects</SelectLabel>
+                      {projects?.map(({ id, name }: Project) => (
+                        <SelectItem
+                          key={id}
+                          className="w-[345px] cursor-default px-3 py-2 text-sm hover:bg-blue-100"
+                          value={id}
+                        >
+                          <SelectItemText>{name}</SelectItemText>
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div>
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" type="text" name="name"/>
+              </div>
             </div>
           </div>
-        </div>
 
-        <DialogFooter className="flex flex-col gap-2 lg:flex-row">
-          <DialogClose asChild>
-            <Button variant="hollow" className="order-2 w-full sm:order-1 sm:w-[130px]">
-              Cancel
+          <DialogFooter className="flex flex-col gap-2 lg:flex-row">
+            <DialogClose asChild>
+              <Button variant="hollow" className="order-2 w-full sm:order-1 sm:w-[130px]">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button variant="default" className="order-1 w-full sm:order-2 sm:w-[130px]">
+              Save
             </Button>
-          </DialogClose>
-          <Button variant="default" className="order-1 w-full sm:order-2 sm:w-[130px]">
-            Save
-          </Button>
-        </DialogFooter>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
