0d1c086dfac932f172f85eda17f0de893123ab4a
josephencila <joseph.mesia.encila@gmail.com>
2024-08-30 17:38:17 +0800

    feat: create loading skeleton and wrap tables

--- apps/codebility/app/home/applicants/_components/applicants-table-container.tsx
+++ apps/codebility/app/home/applicants/_components/applicants-table-container.tsx
@@ -2,12 +2,15 @@
 import { ApplicantsList_Types } from '@/app/home/applicants/_types/applicants'
 import ApplicantsTableDesktop from "@/app/home/applicants/_components/applicants-table-desktop"
 import ApplicantsTableMobile from "@/app/home/applicants/_components/applicants-table-mobile"
-
+import { Suspense } from 'react'
+import ApplicantsLoading from "@/app/home/applicants/loading"
 const ApplicantsTableContainer = ({ applicants }: { applicants: ApplicantsList_Types[] }) => {
     return (
         <>
-            <ApplicantsTableDesktop applicants={applicants} />
-            <ApplicantsTableMobile applicants={applicants} />
+            <Suspense fallback={<ApplicantsLoading />}>
+                <ApplicantsTableDesktop applicants={applicants} />
+                <ApplicantsTableMobile applicants={applicants} />
+            </Suspense>
 
         </>
     )
