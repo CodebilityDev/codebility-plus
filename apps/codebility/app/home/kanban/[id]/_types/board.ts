4c27cbfd9210dfaa5c37ce25549f36ce974afede
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-31 20:11:42 +0800

    feat: list type.

--- apps/codebility/app/home/kanban/[id]/_types/board.ts
+++ apps/codebility/app/home/kanban/[id]/_types/board.ts
@@ -1,7 +1,15 @@
 import type { Project } from "@/types/home/codev";
+import { Task } from "@/types/home/task";
 
 export interface Board {
     id: string;
     name: string;
     project: Project;
+    list: List[];
+}
+
+export interface List {
+    id: string;
+    name: string;
+    task: Task[];
 }
