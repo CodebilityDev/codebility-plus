686d2f8e1157fe1aa0a5bb131f1261b96cc0a301
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-31 20:13:23 +0800

    feat: separate kanban board lists.

--- apps/codebility/app/home/kanban/[id]/_components/kanban-board.tsx
+++ apps/codebility/app/home/kanban/[id]/_components/kanban-board.tsx
@@ -28,19 +28,18 @@ import { kanban_Kanban } from "@/types/protectedroutes"
 import { ListT, TaskT } from "@/types/index"
 import pathsConfig from "@/config/paths.config"
 import { Board } from "../_types/board"
+import KanbanListAddModal from "./kanban-list-add-modal"
+import KanbanBoardListContainer from "./kanban-board-list-container"
 
 interface Props {
   boardData: Board;
 }
 
 const KanbanBoard = ({ boardData }: Props) => {
-/*   const boardId = id */
   const [activeTask, setActiveTask] = useState<TaskT | null>(null)
   const [tasks, setTasks] = useState<TaskT[]>([])
   const [isLoading, setIsLoading] = useState(false)
   const scrollableDiv = useRef<HTMLDivElement>(null)
-/*   const { onOpen } = useModal()
-  const { token } = useToken() */
 
 /*   const columns: ListT[] = useMemo(() => {
     const board = data.find((b) => b.id === boardId)
@@ -90,106 +89,7 @@ const KanbanBoard = ({ boardData }: Props) => {
 
     setIsLoading(false)
   }, [data, boardId])
-
-  useSlider(scrollableDiv, isLoading)
-
-  const sensors = useSensors(
-    useSensor(PointerSensor, {
-      activationConstraint: {
-        distance: 10,
-      },
-    }),
-    useSensor(TouchSensor, {
-      activationConstraint: {
-        delay: 250,
-        tolerance: 5,
-      },
-    })
-  )
-
-  const onDragStart = useCallback((event: DragStartEvent) => {
-    if (event.active.data.current?.type === "Task") {
-      setActiveTask(event.active.data.current.task)
-    }
-  }, [])
- */
-/*   const onDragEnd = useCallback(
-    (event: DragEndEvent) => {
-      setActiveTask(null)
-
-      const { active, over } = event
-      if (!over) return
-
-      const activeId = active.id
-
-      let prevListId = ""
-      columns.forEach((col: ListT) => {
-        if (col.task.some((todo: { id: number }) => todo.id === activeId)) {
-          prevListId = col.id
-        }
-      })
-
-      const activeIndex = tasks.findIndex((t) => t.id === activeId)
-      const newListId = tasks[activeIndex]?.listId
-
-      if (prevListId && newListId && prevListId !== newListId) {
-        const updatedData = {
-          currentListId: prevListId,
-          newListId: newListId,
-          todoOnBoard: [{ todoBoardId: activeId }],
-        }
-
-        updateBoard2(updatedData, token)
-      }
-    },
-    [columns, tasks, token]
-  ) */
-
- /*  const onDragOver = useCallback((event: DragOverEvent) => {
-    const { active, over } = event
-    if (!over) return
-
-    const activeId = active.id
-    const overId = over.id
-
-    if (activeId === overId) return
-
-    const isActiveATask = active.data.current?.type === "Task"
-    const isOverATask = over.data.current?.type === "Task"
-
-    if (!isActiveATask) return
-
-    // Dropping a Task over a Task
-    if (isActiveATask && isOverATask) {
-      setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((t) => t.id === activeId)
-        const overIndex = tasks.findIndex((t) => t.id === overId)
-        const aTask = tasks[activeIndex]
-        const oTask = tasks[overIndex]
-        if (tasks[activeIndex]?.listId !== tasks[overIndex]?.listId) {
-          if (aTask !== undefined && oTask !== undefined) {
-            aTask.listId = oTask.listId
-          }
-        }
-
-        return arrayMove(tasks, activeIndex, overIndex)
-      })
-    }
-
-    const isOverAColumn = over.data.current?.type === "Column"
-
-    if (isActiveATask && isOverAColumn) {
-      setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((t) => t.id === activeId)
-        const aTask = tasks[activeIndex]
-        if (aTask !== undefined) {
-          aTask.listId = overId.toString()
-        }
-
-        return arrayMove(tasks, activeIndex, activeIndex)
-      })
-    }
-  }, []) */
+*/
 
   return (
     <div className="flex h-full w-full">
@@ -217,45 +117,16 @@ const KanbanBoard = ({ boardData }: Props) => {
                   placeholder="Search"
                 />
               </div>
-              <Button variant="default" className="flex" /* onClick={() => onOpen("listAddModal", boardId)} */>
-                <div className="flex items-center gap-2">
-                  <IconAdd />
-                  <p>Add new list</p>
-                </div>
-              </Button>
+              <KanbanListAddModal boardId={boardData.id}/>
             </div>
           </div>
-      {/*     {!isLoading ? (
+          {!isLoading ? (
             <div className="text-dark100_light900 flex h-full">
-              <div className="overflow-x-auto overflow-y-hidden " ref={scrollableDiv}>
-                <DndContext
-                  sensors={sensors}
-                  collisionDetection={pointerWithin}
-                  onDragStart={onDragStart}
-                  onDragEnd={onDragEnd}
-                  onDragOver={onDragOver}
-                >
-                  <ol className="flex w-full gap-2">
-                    {columns.map((col) => (
-                      <ColumnContainer
-                        projectId={projectId}
-                        key={col.id}
-                        column={col}
-                        tasks={tasks.filter((task) => task.listId === col.id)}
-                      />
-                    ))}
-                  </ol>
-                  {typeof window !== "undefined" &&
-                    createPortal(
-                      <DragOverlay>{activeTask && <KanbanTask task={activeTask} listName="" />}</DragOverlay>,
-                      document.body
-                    )}
-                </DndContext>
-              </div>
+              <KanbanBoardListContainer lists={boardData.list} />
             </div>
           ) : (
             "Loading"
-          )} */}
+          )}
         </div>
       </div>
     </div>
