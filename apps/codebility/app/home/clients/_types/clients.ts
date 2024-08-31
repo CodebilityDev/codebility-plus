f5e16d5f60b556bc5e42db0f68220dc87f3d4240
nerwinalamas <nerwinalamas@gmail.com>
2024-08-31 20:42:43 +0800

    feat: client types

---
+++ apps/codebility/app/home/clients/_types/clients.ts
@@ -0,0 +1,13 @@
+
+export type ClientDetails = {
+    id?: string;
+    name: string;
+    logo: null | string;
+    location: string;
+    email: string;
+    contact_number: string;
+    linkedin_link: string;
+    start_time: string;
+    end_time: string;
+    is_archive?: boolean;
+};
