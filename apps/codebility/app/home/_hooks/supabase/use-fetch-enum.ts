d167c753b74e1279937b3220ac4e53760bbe4d88
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-31 20:46:15 +0800

    feat: fetch supabase enum hook.

---
+++ apps/codebility/app/home/_hooks/supabase/use-fetch-enum.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getSupabaseBrowserClient } from "@codevs/supabase/browser-client";
+
+export function useFetchEnum(schema_name: string, enum_name: string) {
+    const client = getSupabaseBrowserClient();
+    
+    const queryFn = async () => {
+        const { data, error } = await client.rpc("get_enum_values", {
+            schema_name,
+            enum_name
+        });
+
+        if (error) throw error;
+
+        return data.map((item: {enum_value: string}) => item.enum_value);
+    }
+
+    return useQuery({
+        queryKey: [`${schema_name}.${enum_name}`],
+        queryFn
+    });
+}
