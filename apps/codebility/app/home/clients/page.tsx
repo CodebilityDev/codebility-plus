39cd43396eb29d2979b2164d84b2e7f64494f0c8
nerwinalamas <nerwinalamas@gmail.com>
2024-08-31 20:39:53 +0800

    refactor: clients page

--- apps/codebility/app/home/clients/page.tsx
+++ apps/codebility/app/home/clients/page.tsx
@@ -1,62 +1,25 @@
-"use client"
+import { getAllClients } from "./service";
+import { ClientDetails } from "./_types/clients";
+import H1 from "@/Components/shared/dashboard/H1";
+import ClientButtons from "./_components/clients-button";
+import ClientCards from "./_components/clients-card";
 
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useQuery, UseQueryResult } from "@tanstack/react-query"
+const Clients = async () => {
+  const data = await getAllClients();
 
-import ClientCard from "@/app/home/clients/ClientCard"
-import { getClients } from "@/app/api/clients"
-import useAuthCookie from "@/hooks/use-cookie"
-import { Button } from "@/Components/ui/button"
-import H1 from "@/Components/shared/dashboard/H1"
-import { useModal } from "@/hooks/use-modal-clients"
-import { client_ClientCardT } from "@/types/protectedroutes"
-import Loading from "@/app/home/clients/loading"
-import Error from "@/app/home/clients/error"
-
-const Clients = () => {
-  const { data: authData } = useAuthCookie()
-  const { userType } = authData || {}
-
-  const { onOpen } = useModal()
-  const router = useRouter()
-
-  const {
-    data: Clients,
-    isLoading: LoadingClients,
-    error: ErrorClients,
-  }: UseQueryResult<client_ClientCardT[]> = useQuery({
-    queryKey: ["clients"],
-    queryFn: async () => {
-      return await getClients()
-    },
-    refetchInterval: 3000,
-  })
-
-  if (LoadingClients) return <Loading />
-
-  if (ErrorClients) return <Error error={ErrorClients} reset={() => getClients()} />
-
-  if (userType?.clients === false) return router.push("/404")
-
-  const archivedClients = Clients?.filter((client) => client?.isArchive === false) || []
+  const clients = (data as ClientDetails[]).filter(
+    (client) => client.is_archive === false
+  );
 
   return (
     <div className="max-w-screen-xl mx-auto flex flex-col gap-4">
-      <div className="flex flex-row justify-between gap-4">
+      <div className="flex flex-col gap-4 md:flex-row md:justify-between">
         <H1>Clients</H1>
-        <div className="flex items-center gap-4">
-          <Link href="/clients/archive">
-            <Button variant="link">Archive</Button>
-          </Link>
-          <Button variant="default" onClick={() => onOpen("clientAddModal")}>
-            Add New Client
-          </Button>
-        </div>
+        <ClientButtons />
       </div>
-      <ClientCard clients={archivedClients as client_ClientCardT[]} />
+      <ClientCards clients={clients} />
     </div>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
