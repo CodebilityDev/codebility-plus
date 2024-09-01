57f1de6ce251fb1ea74438fe8eccd21a9f7741ae
nerwinalamas <nerwinalamas@gmail.com>
2024-09-01 11:15:38 +0800

    styles: add breadcrumbs in archive clients page

--- apps/codebility/app/home/clients/archive/page.tsx
+++ apps/codebility/app/home/clients/archive/page.tsx
@@ -2,6 +2,13 @@ import { getAllClients } from "../service";
 import { ClientDetails } from "../_types/clients";
 import H1 from "@/Components/shared/dashboard/H1"
 import ClientCards from "../_components/clients-card"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@codevs/ui/breadcrumb"
 
 const ClientArchivePage = async () => {
   const data = await getAllClients();
@@ -12,6 +19,15 @@ const ClientArchivePage = async () => {
 
   return (
     <div className="max-w-screen-xl mx-auto flex flex-col gap-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/home/clients">Clients</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>Archive</BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <H1>Archive</H1>
       <ClientCards clients={clients} />
     </div>
