508fb46f6d8ecc39f658bace5a23a6b6fbc283ba
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-01 15:52:24 +0800

    refactor: separate members component in modal.

--- apps/codebility/app/home/kanban/[id]/_components/kanban-task-add-modal.tsx
+++ apps/codebility/app/home/kanban/[id]/_components/kanban-task-add-modal.tsx
@@ -64,10 +64,6 @@ export default function KanbanTaskAddModal({ listName }: Props) {
     points: 0,
     addedMembers: [],
   }) */
-
-  const [searchQuery, setSearchQuery] = useState<string>("")
-  const [selectedMembers, setSelectedMembers] = useState<User[]>([])
-  const [members, setMembers] = useState<User[]>([])
   const { token } = useToken()
   const projectId = data
 
@@ -317,80 +313,6 @@ export default function KanbanTaskAddModal({ listName }: Props) {
               </div>
             </div>
           </div>
-
-          <div className="flex flex-col gap-1">
-            <label htmlFor="members">Members</label>
-            <div className="flex gap-2">
-              <div className="flex flex-wrap items-center">
-                {selectedMembers.map((member, index) => (
-                  <div
-                    className="relative h-12 w-12 cursor-pointer rounded-full bg-cover object-cover"
-                    key={`${member.id}_${index}`}
-           /*          onClick={() => removeMember(member.id)} */
-                  >
-                    <Image
-                      alt="Avatar"
-                      src={member.image_url ?? "/default-avatar.jpg"}
-                      fill
-                      title={`${member.id}'s Avatar`}
-                      className="h-auto w-full rounded-full bg-cover object-cover"
-                      loading="eager"
-                    />
-                  </div>
-                ))}
-              </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger className="cursor-pointer">
-                  <Button variant="hollow" className="h-12 w-12 rounded-full p-0">
-                    <IconPlus className="invert dark:invert-0" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  side="bottom"
-                  sideOffset={10}
-                  align="start"
-                  className="z-10 max-h-[200px] overflow-y-auto rounded-lg bg-white dark:bg-dark-100"
-                >
-                  <DropdownMenuLabel className="pb-2 text-center text-sm">Add Members</DropdownMenuLabel>
-                  <input
-                    type="text"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    placeholder="Search members"
-                    className="border-gray-300 mb-2 h-10 w-full rounded-lg border bg-white px-3 text-sm focus:outline-none dark:bg-dark-200"
-                  />
-                  <DropdownMenuSeparator />
-                  <DropdownMenuLabel className="px-4 py-2 text-xs">Available Members</DropdownMenuLabel>
-                  {members
-                    ?.filter((user) =>
-                      `${user.first_name} ${user.last_name}`.toLowerCase().includes(searchQuery.toLowerCase())
-                    )
-                    .map((user) => (
-                      <DropdownMenuItem
-                        key={user.id}
-                        className="hover:bg-gray-100 flex cursor-pointer items-center justify-between px-4 py-2 dark:hover:bg-dark-200"
-                      /*   onClick={() => addMember(user)} */
-                      >
-                        <div className="flex items-center gap-2">
-                          <div className="relative h-8 w-8 rounded-full bg-cover object-cover">
-                            <Image
-                              alt="Avatar"
-                              src={user.image_url ?? "/default-avatar.jpg"}
-                              fill
-                              title={`${user.id}'s Avatar`}
-                              className="h-auto w-full rounded-full bg-cover object-cover"
-                              loading="eager"
-                            />
-                          </div>
-                          <span>{`${user.first_name} ${user.last_name}`}</span>
-                        </div>
-                      </DropdownMenuItem>
-                    ))}
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </div>
-
           <div className="flex flex-col gap-1">
             <Label htmlFor="desc">Description</Label>
             <Textarea
