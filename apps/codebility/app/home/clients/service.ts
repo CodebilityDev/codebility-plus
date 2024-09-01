ccde513ebf26cf69619c799fbdb4e58be785ce70
nerwinalamas <nerwinalamas@gmail.com>
2024-09-01 13:04:27 +0800

    refactor: get all service with image

--- apps/codebility/app/home/clients/service.ts
+++ apps/codebility/app/home/clients/service.ts
@@ -5,13 +5,19 @@ import { createServer } from "@/utils/supabase";
 export const getAllClients = async () => { 
     const supabase = createServer();
     const { data, error } = await supabase
-    .from("clients")
-    .select("*");
+        .from("clients")
+        .select("*");
 
     if (error) {
         console.error("Error fetching clients:", error.message);
         return { success: false, error: error.message };
     }
 
+    data.forEach(client => {
+        if (client.logo) {
+            client.logo = supabase.storage.from("clients-image").getPublicUrl(client.logo).data.publicUrl;
+        }
+    });
+    
     return data;
-}
+};
