02f38b54b365820b6888a02d90573ea25457b812
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-01 21:39:16 +0800

    fix: multiple instance of supabase browser client getting created.

--- packages/supabase/src/clients/browser.client.ts
+++ packages/supabase/src/clients/browser.client.ts
@@ -2,12 +2,13 @@ import { createClient } from "@supabase/supabase-js";
 
 import { getSupabaseClientKeys } from '../get-supabase-client-keys';
 
+const keys = getSupabaseClientKeys();
+const client = createClient(keys.url, keys.anonKey);
+
 /**
  * @name getSupabaseBrowserClient
  * @description Get a Supabase client for use in the Browser
  */
 export function getSupabaseBrowserClient() {
-  const keys = getSupabaseClientKeys();
-
-  return createClient(keys.url, keys.anonKey);
+  return client;
 }
