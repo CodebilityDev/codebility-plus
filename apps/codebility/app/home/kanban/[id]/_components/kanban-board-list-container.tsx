905ddc35221c8455cb080de2c72299958f602589
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-02 14:43:34 +0800

    refactor: make drag over works again.

--- apps/codebility/app/home/kanban/[id]/_components/kanban-board-list-container.tsx
+++ apps/codebility/app/home/kanban/[id]/_components/kanban-board-list-container.tsx
@@ -27,9 +27,11 @@ interface Props {
 }
 
 export default function KanbanBoardListContainer({ lists, projectId }: Props) {
-  const [activeTask, setActiveTask] = useState<Task | null>(null)
   const scrollableDiv = useRef<HTMLDivElement>(null)
-/*   const [tasks, setTasks] = useState<Task[]>() */
+  const [tasks, setTasks] = useState<Task[]>((lists.reduce((total: Task[], list: List) => {
+    if (Array.isArray(list.task)) total.push(...list.task);
+    return total;
+  }, [])));
 
   useSlider(scrollableDiv, false)
 
@@ -79,7 +81,7 @@ export default function KanbanBoardListContainer({ lists, projectId }: Props) {
     [columns, tasks, token]
   ) */
 
- /*  const onDragOver = useCallback((event: DragOverEvent) => {
+  const onDragOver = useCallback((event: DragOverEvent) => {
     const { active, over } = event
     if (!over) return
 
@@ -96,13 +98,18 @@ export default function KanbanBoardListContainer({ lists, projectId }: Props) {
     // Dropping a Task over a Task
     if (isActiveATask && isOverATask) {
       setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((t) => t.id === activeId)
+        // find the index of current dragged element in the tasks array.
+        const activeIndex = tasks.findIndex((t) => t.id === activeId) 
+        // find the index of an element where the current dragged is over in the tasks array.
         const overIndex = tasks.findIndex((t) => t.id === overId)
         const aTask = tasks[activeIndex]
         const oTask = tasks[overIndex]
-        if (tasks[activeIndex]?.listId !== tasks[overIndex]?.listId) {
+
+        // check if they are not in the same column
+        if (tasks[activeIndex]?.list_id !== tasks[overIndex]?.list_id) {
           if (aTask !== undefined && oTask !== undefined) {
-            aTask.listId = oTask.listId
+            // make the dragged element be in the same column of where it is over.
+            aTask.list_id = oTask.list_id
           }
         }
 
@@ -110,38 +117,37 @@ export default function KanbanBoardListContainer({ lists, projectId }: Props) {
       })
     }
 
+
+    // if a column have an empty task and we want to put the task we are dragging to.
     const isOverAColumn = over.data.current?.type === "Column"
 
     if (isActiveATask && isOverAColumn) {
       setTasks((tasks) => {
         const activeIndex = tasks.findIndex((t) => t.id === activeId)
         const aTask = tasks[activeIndex]
-        if (aTask !== undefined) {
-          aTask.listId = overId.toString()
+        if (aTask) {
+          // assign the dragged task the list id of the column its on.
+          aTask.list_id = overId.toString() 
         }
-
         return arrayMove(tasks, activeIndex, activeIndex)
       })
     }
-  }, []) */
+  }, [])
   return (
     <div className="overflow-x-auto overflow-y-hidden " ref={scrollableDiv}>
       <DndContext
         sensors={sensors}
         collisionDetection={pointerWithin}
-       /*  onDragStart={onDragStart} */
-      /*   onDragEnd={onDragEnd}
-        onDragOver={onDragOver} */
+        // onDragEnd={onDragEnd}
+        onDragOver={onDragOver}
       >
         <ol className="flex w-full gap-2">
           {lists.map((col) => (
             <KanbanColumnContainer
               column={col}
-              /* projectId={projectId} */
+              tasks={tasks.filter(task => task.list_id === col.id)}
               key={col.id}
               projectId={projectId}
-           /*    column={col}
-              tasks={tasks.filter((task) => task.listId === col.id)} */
             />
           ))}
         </ol>
