0753197e9d79fb96be41c5e3d611d7d555bb8b5d
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-02 14:59:36 +0800

    refactor: clean unused codes.

--- apps/codebility/app/home/kanban/[id]/_components/kanban-board.tsx
+++ apps/codebility/app/home/kanban/[id]/_components/kanban-board.tsx
@@ -1,31 +1,8 @@
 "use client"
-import React, { useState, useMemo, useEffect, useRef, useCallback } from "react"
-import { createPortal } from "react-dom"
-import {
-  DndContext,
-  DragEndEvent,
-  DragOverlay,
-  DragStartEvent,
-  useSensor,
-  useSensors,
-  PointerSensor,
-  TouchSensor,
-  DragOverEvent,
-  pointerWithin,
-} from "@dnd-kit/core"
+import React, { useState } from "react"
 import Link from "next/link"
-import { updateBoard2 } from "@/app/api/kanban"
-import useToken from "@/hooks/use-token"
-import useSlider from "@/hooks/useSlider"
-import { useModal } from "@/hooks/use-modal"
-import { arrayMove } from "@dnd-kit/sortable"
-import { Button } from "@/Components/ui/button"
-import KanbanTask from "./kanban-task"
 import { ArrowRightIcon } from "@/public/assets/svgs"
-import { IconAdd, IconSearch } from "@/public/assets/svgs"
-import ColumnContainer from "./kanban-column-container"
-import { kanban_Kanban } from "@/types/protectedroutes"
-import { ListT, TaskT } from "@/types/index"
+import { IconSearch } from "@/public/assets/svgs"
 import pathsConfig from "@/config/paths.config"
 import { Board } from "../_types/board"
 import KanbanListAddModal from "./kanban-list-add-modal"
@@ -36,60 +13,7 @@ interface Props {
 }
 
 const KanbanBoard = ({ boardData }: Props) => {
-  const [activeTask, setActiveTask] = useState<TaskT | null>(null)
-  const [tasks, setTasks] = useState<TaskT[]>([])
   const [isLoading, setIsLoading] = useState(false)
-  const scrollableDiv = useRef<HTMLDivElement>(null)
-
-/*   const columns: ListT[] = useMemo(() => {
-    const board = data.find((b) => b.id === boardId)
-    return board ? board.lists : []
-  }, [data, boardId])
-
-  const boardName: string = useMemo(() => {
-    const board = data.find((b) => b.id === boardId)
-    return board ? board.name : ""
-  }, [data, boardId])
-
-  const projectId: string = useMemo(() => {
-    const board = data.find((b) => b.id === boardId)
-
-    if (board && board.boardProjects && board.boardProjects.length > 0) {
-      const projectsId = board.boardProjects[0]?.projectsId
-      if (projectsId !== undefined) {
-        return projectsId
-      } else {
-        return ""
-      }
-    } else {
-      return ""
-    }
-  }, [data, boardId]) */
-
-  /* useEffect(() => {
-    setIsLoading(true)
-
-    const board = data.find((b) => b.id === boardId)
-
-    if (board && Array.isArray(board.lists)) {
-      const tempTasks = board.lists.reduce((acc: TaskT[], col: ListT) => {
-        if (Array.isArray(col.task)) {
-          // <-- Error here
-          acc.push(...col.task)
-        }
-        return acc
-      }, [])
-
-      if (tempTasks) {
-        setTasks(tempTasks)
-      }
-    } else {
-      setTasks([])
-    }
-
-    setIsLoading(false)
-  }, [data, boardId])
-*/
 
   return (
     <div className="flex h-full w-full">
