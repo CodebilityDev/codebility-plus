8104996c94fa02661d72e454ca1f6eb437dc66e9
nerwinalamas <nerwinalamas@gmail.com>
2024-09-02 22:57:48 +0800

    refactor: get all categories

--- apps/codebility/app/home/settings/services/add-new-service/page.tsx
+++ apps/codebility/app/home/settings/services/add-new-service/page.tsx
@@ -1,16 +1,19 @@
 import { createServer } from "@/utils/supabase";
 import ServiceForm from "../_components/service-form";
+import { getAllServiceCategories } from "../categories/service";
+import { Category } from "../categories/_types/category";
 
  const AddNewService = async () => {
   const supabase = createServer();
   const { data: { user }, error } = await supabase.auth.getUser();
+  const categories = await getAllServiceCategories(); 
 
   if (error) {
     console.error("Error fetching user:", error);
     return <p>Error fetching user</p>;
   }
 
-  return <ServiceForm userId={user?.id} />
+  return <ServiceForm userId={user?.id} categories={categories as Category[]} />
 }
 
 export default AddNewService
