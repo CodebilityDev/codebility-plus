b062dfb06ef23e18c40cba74d640b4bacc7be3f7
nerwinalamas <nerwinalamas@gmail.com>
2024-09-02 22:58:44 +0800

    refactor: service category page

--- apps/codebility/app/home/settings/services/categories/page.tsx
+++ apps/codebility/app/home/settings/services/categories/page.tsx
@@ -1,83 +1,30 @@
 import { H1 } from "@/Components/shared/dashboard"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from "@codevs/ui/breadcrumb"
-import { Button } from "@/Components/ui/button"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/Components/ui/table"
-import { IconDelete, IconEdit } from "@/public/assets/svgs"
+import CustomBreadcrumb from "@/Components/shared/dashboard/CustomBreadcrumb"
+import CategoriesAddNew from "./_components/categories-add-new"
+import CategoriesTable from "./_components/categories-table"
+import { getAllServiceCategories } from "./service"
+import { Category } from "./_types/category"
 
-const services = [
-  {
-    name: "Web Application",
-  },
-  {
-    name: "Mobile Application",
-  },
-  {
-    name: "Product Design",
-  },
+const items = [
+  { label: "Settings", href: "/home/settings" },
+  { label: "Services", href: "/home/settings/services" },
+  { label: "Categories" }
 ]
 
-const ServiceCategories = () => {
-  return (
-    <>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/settings">Settings</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/settings/services">Services</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>Categories</BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <div className="flex flex-col gap-4 pt-4">
-        <div className="flex flex-row justify-between gap-4">
-          <H1>Service Categories</H1>
-          <div className="flex gap-4">
-            <Button variant="default" size="lg" className="w-[200px]">
-              Add New Category
-            </Button>
-          </div>
-        </div>
-        <Table className="background-box text-dark100_light900 mt-8 w-full  table-fixed overflow-auto">
-          <TableHeader className="background-lightbox_darkbox border-top text-xl">
-            <TableRow>
-              <TableHead className="table-border-light_dark w-full border-b-[1px] p-4 text-left text-xl font-light">
-                Name
-              </TableHead>
+const ServiceCategories = async () => {
+  const categories = await getAllServiceCategories();
 
-              <TableHead className="table-border-light_dark w-20 border-b-[1px] p-4 text-left text-xl font-light">
-                Edit
-              </TableHead>
-              <TableHead className="table-border-light_dark w-36 border-b-[1px] p-4 text-left text-xl font-light">
-                Delete
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {services.map((service, index) => (
-              <TableRow key={index} className={`odd:bg-gray-100 even:bg-[#444857]`}>
-                <TableCell className="p-4 text-xl font-light">{service.name}</TableCell>
-                <TableCell className="p-4 text-xl font-light">
-                  <IconEdit className="mx-auto cursor-pointer duration-100 hover:scale-105" />
-                </TableCell>
-                <TableCell className="p-4 text-xl font-light">
-                  <IconDelete className="ms-4 cursor-pointer text-blue-100 duration-100 hover:scale-105" />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <CustomBreadcrumb items={items} />
+      <div className="flex flex-col gap-4 pt-4">
+        <div className="flex flex-col justify-between gap-4 md:flex-row md:justify-between md:items-center">
+          <H1>Service Categories</H1>
+          <CategoriesAddNew />
+        </div>
+        <CategoriesTable categories={categories as Category[]} />
       </div>
-    </>
+    </div>
   )
 }
 
