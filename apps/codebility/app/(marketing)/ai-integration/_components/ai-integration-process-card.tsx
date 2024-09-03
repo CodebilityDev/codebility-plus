c8b67009a61e00db4d3f62fbe8ba9afbbd61a783
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-04 01:49:00 +0800

    refactor: bring props type closer to component because they are not shared.

--- apps/codebility/app/(marketing)/ai-integration/_components/ai-integration-process-card.tsx
+++ apps/codebility/app/(marketing)/ai-integration/_components/ai-integration-process-card.tsx
@@ -1,6 +1,10 @@
-import { ai_ProcessCardT } from "@/types/home"
+interface Props { 
+  id: string;
+  title: string;
+  process: string[]
+}
 
-const ProcessCard = ({ id, title, process }: ai_ProcessCardT) => {
+const ProcessCard = ({ id, title, process }: Props) => {
   return (
     <div className="-mt-5 flex flex-col gap-3 text-white">
       <p className="flex h-12 w-12 items-center justify-center rounded-full bg-[#9747FF] text-xl font-medium">{id}</p>
