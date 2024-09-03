e972df85da18ca338a41b78a423b02b29cd700c7
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-03 11:01:37 +0800

    feat: filter list with search.

--- apps/codebility/app/home/kanban/[id]/page.tsx
+++ apps/codebility/app/home/kanban/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { getSupabaseServerComponentClient } from "@codevs/supabase/server-component-client"
 import KanbanBoard from "./_components/kanban-board"
+import { List } from "./_types/board";
 
-export default async function KanbanPage({ params }: { params: { id: string } }) {
+export default async function KanbanPage({ params, searchParams }: 
+  { 
+    params: { id: string },
+    searchParams: { query: string }  
+  }) {
   const supabase = getSupabaseServerComponentClient();
 
   const { data: board, error } = await supabase.from("board")
@@ -25,5 +30,11 @@ export default async function KanbanPage({ params }: { params: { id: string } })
   `)
   .eq("id", params.id)
   .single();
+  
+  const listQuery = searchParams.query;
+
+  if (listQuery) // filter out board lists by the search input value.
+    board.list = board.list.filter((l: List) => l.name.indexOf(listQuery) >= 0);
+
   return <KanbanBoard boardData={board}/>
 }
