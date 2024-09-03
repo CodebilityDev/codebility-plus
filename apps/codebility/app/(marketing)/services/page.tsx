07326f545bd74a046561ce944cb55b5db2a9aff3
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-03 19:02:27 +0800

    update: use shared lib.

--- apps/codebility/app/(marketing)/services/page.tsx
+++ apps/codebility/app/(marketing)/services/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "../_components/marketing-footer"
 import Hero from "./components/Hero"
 import Navigation from "../_components/marketing-navigation"
-import { PoppinFont } from "@/app/(marketing)/index/Index"
+import { PoppinFont } from "../_lib/font"
 import Calendly from "@/app/(marketing)/index/components/Calendly"
 import ServicesTab from "./components/ServicesTab"
 
