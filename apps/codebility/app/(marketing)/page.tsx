7dba08dc398374cb63d247c473a36f8b7935998a
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-03 20:55:22 +0800

    refactor: move landing page main entry.

--- apps/codebility/app/(marketing)/page.tsx
+++ apps/codebility/app/(marketing)/page.tsx
@@ -1,5 +1,30 @@
-import Index from "@/app/(marketing)/index"
+import Navigation from "./_components/marketing-navigation"
+import Footer from "./_components/marketing-footer"
+import Hero from "./_components/landing/landing-hero"
+import Admins from "./_components/landing/landing-admins"
+import Parallax from "./_components/landing/landing-parallax"
+import Calendly from "./_components/marketing-calendly"
+import Features from "./_components/landing/landing-features"
+import WorkWithUs from "./_components/landing/landing-work-with-us"
+import WhyChooseUs from "./_components/landing/landing-why-choose-us"
+import { PoppinFont } from "./_lib/font"
 
-export default function MarketingPage() {
-  return (<Index />);
+const Index = () => {
+  return (
+    <div
+      className={`relative flex w-full flex-col overflow-x-hidden overflow-y-hidden bg-[#030303] ${PoppinFont.className}`}
+    >
+      <Navigation />
+      <Hero />
+      <Features />
+      <Parallax />
+      <WhyChooseUs />
+      <WorkWithUs />
+      <Admins />
+      <Calendly />
+      <Footer />
+    </div>
+  )
 }
+
+export default Index
