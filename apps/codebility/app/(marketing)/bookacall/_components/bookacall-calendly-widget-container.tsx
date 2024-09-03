12f6bdc88d2109612229c6a3e5b87f6f3f9ffa30
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-04 01:53:51 +0800

    feat: calendly container.

---
+++ apps/codebility/app/(marketing)/bookacall/_components/bookacall-calendly-widget-container.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { InlineWidget } from 'react-calendly'
+
+export default function BookACallCalendlyWidgetContainer() {
+  return (
+    <div className="calendly-widget-container w-full justify-center">
+          <InlineWidget url="https://calendly.com/codebility-dev/30min" />
+    </div>
+  )
+}
