beeca09bb5d060f6bbdcc1d483873bab39f75bf1
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-03 10:54:28 +0800

    feat: make component shareable.

--- apps/codebility/app/home/kanban/_components/kanban-boards-search.tsx
+++ apps/codebility/app/home/kanban/_components/kanban-boards-search.tsx
@@ -2,7 +2,12 @@
 
 import { usePathname, useSearchParams, useRouter } from "next/navigation"
 
-export default function KanbanBoardsSearch() {
+interface Props {
+  className: string;
+  placeholder: string;
+}
+
+export default function KanbanBoardsSearch({ className, placeholder }: Props) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -18,10 +23,10 @@ export default function KanbanBoardsSearch() {
   return (
     <input
       type="text"
-      placeholder="Search board"
+      placeholder={placeholder}
       onChange={(e) => handleSearch(e.target.value)}
       defaultValue={searchParams.get("query")?.toString()}
-      className="h-10 w-full rounded-full border border-gray border-opacity-50 bg-inherit px-5 text-xs focus:outline-none md:w-80"
+      className={className}
     />
   )
 }
