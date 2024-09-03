ec845b785bb999a9360f72a9af9647e9b524a169
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-04 02:11:30 +0800

    refactor: bring prop close to its component because it was not shared.

--- apps/codebility/app/(marketing)/services/components/index.ts
+++
@@ -1,9 +0,0 @@
-import { StaticImageData } from "next/image"
-
-export type ServiceTabCardProps = {
-  id?: string
-  projectName: string
-  description?: string
-  projectImage?: StaticImageData | string
-  projectType?: string
-}
