4f1d00768a8680583bd7c927b9a3235ca3ac5322
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-04 13:23:50 +0800

    style: make black background take full width.

--- apps/codebility/app/(marketing)/_components/marketing-navigation.tsx
+++ apps/codebility/app/(marketing)/_components/marketing-navigation.tsx
@@ -16,39 +16,41 @@ const Navigation = () => {
   return (
     <>
       <div
-        className={`fixed left-1/2 z-50 flex w-full max-w-screen-2xl -translate-x-1/2 items-center justify-between p-5 lg:px-12 ${
+        className={`fixed z-50 flex w-full items-center justify-center p-5 lg:px-12 ${
           color ? "bg-[#030303]" : ""
         }`}
       >
-        <Logo />
+        <div className="flex justify-between w-full max-w-screen-2xl items-center">
+          <Logo />
 
-        <div className="flex items-center">
-          <Link href="/bookacall">
-            <Button variant="purple" rounded="full" size="lg" className="hidden lg:block">
-              Let{`'`}s Connect
-            </Button>
-          </Link>
+          <div className="flex items-center">
+            <Link href="/bookacall">
+              <Button variant="purple" rounded="full" size="lg" className="hidden lg:block">
+                Let{`'`}s Connect
+              </Button>
+            </Link>
 
-          <Sheet open={openSheet} onOpenChange={setOpenSheet}>
-            <SheetTrigger>
-              <IconFourDotsMenu className="lg:hidden" />
-            </SheetTrigger>
-            <SheetContent
-              side="top"
-              className="flex h-96 w-full flex-col items-center justify-center border-none text-white"
-            >
-              {navItems.map((item, index) => (
-                <Link
-                  onClick={() => setOpenSheet(false)}
-                  href={item.path}
-                  key={item.id}
-                  className={`w-full ${index === 3 ? "border-none" : "border-b border-black-100"}`}
-                >
-                  <p className="w-full cursor-pointer px-2 py-3 text-center text-base">{item.title}</p>
-                </Link>
-              ))}
-            </SheetContent>
-          </Sheet>
+            <Sheet open={openSheet} onOpenChange={setOpenSheet}>
+              <SheetTrigger>
+                <IconFourDotsMenu className="lg:hidden" />
+              </SheetTrigger>
+              <SheetContent
+                side="top"
+                className="flex h-96 w-full flex-col items-center justify-center border-none text-white"
+              >
+                {navItems.map((item, index) => (
+                  <Link
+                    onClick={() => setOpenSheet(false)}
+                    href={item.path}
+                    key={item.id}
+                    className={`w-full ${index === 3 ? "border-none" : "border-b border-black-100"}`}
+                  >
+                    <p className="w-full cursor-pointer px-2 py-3 text-center text-base">{item.title}</p>
+                  </Link>
+                ))}
+              </SheetContent>
+            </Sheet>
+          </div>
         </div>
       </div>
     </>
