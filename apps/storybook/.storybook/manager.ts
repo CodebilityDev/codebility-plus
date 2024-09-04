e0fbe3791e8a28ed53581d77ffc8b036b863ee02
Nemuel M Gabitanan Jr <nemuelmgabitananjr@gmail.com>
2024-09-05 00:57:11 +0800

    fix: storybook global theme update

--- apps/storybook/.storybook/manager.ts
+++ apps/storybook/.storybook/manager.ts
@@ -12,10 +12,23 @@ addons.setConfig({
     //Logo section
     brandTitle: 'Codebility',
     brandUrl: 'https://staging.codebility.tech/',
-    brandImage: '/assets/images/codebility.png',
+    brandImage: '/assets/images/codebility.jpg',
     brandTarget: '_blank',
 
-    colorPrimary: '#000000',
+    colorPrimary: '#F5F5F5',
     colorSecondary: '#9747FF',
+
+    // UI
+    appBg: '#000000',
+    appContentBg: '#232323',
+    appPreviewBg: '#F5F5F5',
+    appBorderColor: '#F5F5F5',
+    appBorderRadius: 4,
+
+    // Form colors
+    inputBg: '#F5F5F5',
+    inputBorder: '#000000',
+    inputTextColor: '#000000',
+    inputBorderRadius: 2,
   })
 })
