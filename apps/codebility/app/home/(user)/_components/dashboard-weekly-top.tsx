b3f981ab5344ab86b6154bdd3ce18407a3f716c9
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-04 21:10:47 +0800

    feat: display error if error occured.

--- apps/codebility/app/home/(user)/_components/dashboard-weekly-top.tsx
+++ apps/codebility/app/home/(user)/_components/dashboard-weekly-top.tsx
@@ -30,52 +30,55 @@ export default async function WeeklyTop() {
           </TableHeader>
           <TableBody>
             {
-              data.map(({ id, first_name, main_position, level }: TopNotcher, index: number) => {
-                const placement = index + 1;
+              error ?
+                <div>ERROR</div>
+                :
+                data.map(({ id, first_name, main_position, level }: TopNotcher, index: number) => {
+                  const placement = index + 1;
 
-                const AbbreviationMap = {
-                  "Fullstack Developer": "FS",
-                  "Front End Developer": "FE",
-                  "Back End Developer": "BE",
-                  "Mobile Developer": "M",
-                  "Admin": "AD",
-                }
+                  const AbbreviationMap = {
+                    "Fullstack Developer": "FS",
+                    "Front End Developer": "FE",
+                    "Back End Developer": "BE",
+                    "Mobile Developer": "M",
+                    "Admin": "AD",
+                  }
 
-                const role = AbbreviationMap[main_position as keyof typeof AbbreviationMap] || main_position;
+                  const role = AbbreviationMap[main_position as keyof typeof AbbreviationMap] || main_position;
 
-                return <TableRow
-                  key={id}
-                  className={`grid grid-cols-3 gap-3 md:grid-cols-4 ${
-                    placement == 1
-                      ? "bg-gradient-to-r from-[#9c813b] to-[#ecc258] text-white"
-                      : placement == 2
-                      ? "bg-gradient-to-r from-[#464646] to-[#a8a8a8] text-white"
-                      : placement == 3 && "bg-gradient-to-r from-[#563c1e] to-[#ba8240] text-white"
-                  }`}
-                >
-                  <TableCell className="flex justify-center text-center">
-                    {" "}
-                    {placement == 1 ? (
-                      <FEChampion className="h-[1.8rem] w-[1.8rem]" />
-                    ) : placement == 2 ? (
-                      <FE1stRunnerUp className="h-[1.7rem] w-[1.7rem]" />
-                    ) : placement == 3 ? (
-                      <FE2ndRunnerUp className="h-[1.7rem] w-[1.7rem]" />
-                    ) : (
-                      placement
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    <p className="capitalize">{first_name}</p>
-                  </TableCell>
-                  <TableCell className="text-center">
-                    <p>{role}</p>
-                  </TableCell>
-                  <TableCell className="hidden text-center md:block">
-                    <p>{level}</p>
-                  </TableCell>
-                </TableRow>
-          })}
+                  return <TableRow
+                    key={id}
+                    className={`grid grid-cols-3 gap-3 md:grid-cols-4 ${
+                      placement == 1
+                        ? "bg-gradient-to-r from-[#9c813b] to-[#ecc258] text-white"
+                        : placement == 2
+                        ? "bg-gradient-to-r from-[#464646] to-[#a8a8a8] text-white"
+                        : placement == 3 && "bg-gradient-to-r from-[#563c1e] to-[#ba8240] text-white"
+                    }`}
+                  >
+                    <TableCell className="flex justify-center text-center">
+                      {" "}
+                      {placement == 1 ? (
+                        <FEChampion className="h-[1.8rem] w-[1.8rem]" />
+                      ) : placement == 2 ? (
+                        <FE1stRunnerUp className="h-[1.7rem] w-[1.7rem]" />
+                      ) : placement == 3 ? (
+                        <FE2ndRunnerUp className="h-[1.7rem] w-[1.7rem]" />
+                      ) : (
+                        placement
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <p className="capitalize">{first_name}</p>
+                    </TableCell>
+                    <TableCell className="text-center">
+                      <p>{role}</p>
+                    </TableCell>
+                    <TableCell className="hidden text-center md:block">
+                      <p>{level}</p>
+                    </TableCell>
+                  </TableRow>
+            })}
           </TableBody>
         </Table>
       </div>
