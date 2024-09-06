e7c955fe0bdc819e79d40074b57b54c2c69ac29c
josephencila <joseph.mesia.encila@gmail.com>
2024-09-06 22:01:28 +0800

    fix and refactor: hydration in action button with span

--- apps/codebility/app/home/applicants/_components/applicants-action-buttons.tsx
+++ apps/codebility/app/home/applicants/_components/applicants-action-buttons.tsx
@@ -33,20 +33,18 @@ const ApplicantsApprovalButtons = ({ email_address }: { email_address: string })
 
     return (
         <>
-            <button
-                className="h-max w-max rounded-md bg-blue-100 px-4 py-1 text-white transition duration-300 hover:bg-blue-500"
-                onClick={() => handleAccept(email_address as string)}
-                disabled={isLoading}
+            <span
+                className="cursor-pointer h-max w-max rounded-md bg-blue-100 px-4 py-1 text-white transition duration-300 hover:bg-blue-500"
+                onClick={isLoading ? undefined : () => handleAccept(email_address as string)}
             >
                 Accept
-            </button>
-            <button
-                className="h-max w-max rounded-md border border-none px-4 py-1 hover:underline"
-                onClick={() => handleDeny(email_address as string)}
-                disabled={isLoading}
+            </span>
+            <span
+                className="cursor-pointer h-max w-max rounded-md border border-none px-4 py-1 hover:underline"
+                onClick={isLoading ? undefined : () => handleDeny(email_address as string)}
             >
                 Deny
-            </button>
+            </span>
         </>
     );
 };
