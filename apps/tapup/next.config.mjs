1a586dbbb43793db6f7abd4eacbe34d021f23459
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-06 19:01:15 +0800

    fix: check if split value exits.

--- apps/tapup/next.config.mjs
+++ apps/tapup/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL.split("https://")[1] : "https://nwpvsxbrftplvebseaas.supabase.co",
+        hostname: (process.env.NEXT_PUBLIC_SUPABASE_URL && process.env.NEXT_PUBLIC_SUPABASE_URL.split("https://")[1]) ?? "https://nwpvsxbrftplvebseaas.supabase.co",
       },
       {
         protocol: "https",
