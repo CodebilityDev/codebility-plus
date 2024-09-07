2874ff992babe3f61876b257300d4e55f5a41ab0
ciscutiez <francischarlesi.gonzales@gmail.com>
2024-09-07 17:38:50 +0800

    feat and fix: avatar upload and remove working, time schedule working, refactor and fix some functions

--- apps/codebility/Components/time-picker/time-picker-12hour-wrapper.tsx
+++ apps/codebility/Components/time-picker/time-picker-12hour-wrapper.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import * as React from "react"
-import { TimePicker12Demo } from "@/Components/time-picker/time-picker-12hour-demo"
+// import * as React from "react"
+// import { TimePicker12Demo } from "@/Components/time-picker/time-picker-12hour-demo"
 
 export function TimePicker12HourWrapper() {
-  const [date, setDate] = React.useState<Date>()
-  return <TimePicker12Demo setDate={setDate} date={date} />
+  // const [date, setDate] = React.useState<Date>()
+  // return <TimePicker12Demo setDate={setDate} date={date} />
 }
---
+++ apps/codebility/app/home/settings/resume/_types/resume.ts
@@ -0,0 +1,20 @@
+export type Profile_Types = {
+    id?: string
+    first_name?: string
+    last_name?: string 
+    about?: string
+    address?:string
+    tech_stack?: string
+  }
+  export type Social_Types = {
+    id: string,
+    phone_no: string
+    fb_link: string
+    linkedin_link: string
+    whatsapp_link: string
+    skype_link: string
+    telegram_link: string
+    portfolio_website: string
+    user_id: string
+    github_link: string
+  }
