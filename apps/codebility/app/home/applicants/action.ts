0865da24a8fb93c61ba3e6de21cbe1437248ecef
josephencila <joseph.mesia.encila@gmail.com>
2024-09-07 14:08:32 +0800

    fix: applicant edit modal, action malformed data

--- apps/codebility/app/home/applicants/action.ts
+++ apps/codebility/app/home/applicants/action.ts
@@ -4,11 +4,14 @@ import { revalidatePath } from "next/cache";
 
 import { getSupabaseServerActionClient } from "@codevs/supabase/server-actions-client";
 
-import { ApplicantsFormValues } from "./_lib/applicants-schema";
 import { ApplicantsList_Types } from "./_types/applicants";
 
 const supabase = getSupabaseServerActionClient();
 const revalidate = () => revalidatePath("/home/applicants");
+
+const toArray = (str: string) => {
+  return str.split(",").map((item) => item.trim()); // Split by comma and trim spaces
+};
 
 export async function rejectAction(email_address: string) {
   try {
@@ -90,7 +93,7 @@ export async function updateAction(id: string, formData: FormData) {
   const email_address = formData.get("email_address") as string;
   const github_link = formData.get("github_link") as string;
   const portfolio_website = formData.get("portfolio_website") as string;
-  const tech_stacks = formData.get("tech_stacks") as string;
+  const tech_stacks = formData.get("tech_stacks") as any;
 
   try {
     const { data: applicantExist, error: fetchError } = await supabase
@@ -109,7 +112,7 @@ export async function updateAction(id: string, formData: FormData) {
         email_address,
         github_link,
         portfolio_website,
-        tech_stacks,
+        tech_stacks: toArray(tech_stacks),
       })
       .eq("id", id)
       .single();
