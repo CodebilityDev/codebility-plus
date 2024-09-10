ccae98f187ccf00eefb928c458ec0d6d7e303eba
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-10 09:28:11 +0800

    feat: use path config.

--- apps/codebility/middleware.ts
+++ apps/codebility/middleware.ts
@@ -28,63 +28,20 @@ const getUser = (req: NextRequest, res: NextResponse) => {
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
-
   // handle patterns for specific routes
-  // const handlePattern = matchUrlPattern(req.url);
+  const handlePattern = matchUrlPattern(req.url);
 
-  // // if a pattern handler exists, call it
-  // if (handlePattern) {
-  //   const patternHandlerResponse = await handlePattern(req, new NextResponse());
-  // // if a pattern handler exists, call it
-  // if (handlePattern) {
-  //   const patternHandlerResponse = await handlePattern(req, new NextResponse());
+  // if a pattern handler exists, call it
+  if (handlePattern) {
+    const patternHandlerResponse = await handlePattern(req, new NextResponse());
 
-  //   // if a pattern handler returns a response, return it
-  //   if (patternHandlerResponse) {
-  //     return patternHandlerResponse;
-  //   }
-  // }
-
-  // return res;
-
-  ///////// OLD CODEBILITY MIDDLEWARE //////////// - uncomment this and comment the above code if using old be.
-/*   const { nextUrl, cookies } = req
-  if (!cookies.has("codebility-auth")) {
-    return NextResponse.redirect(new URL("/auth/signin", req.url))
+    // if a pattern handler returns a response, return it
+    if (patternHandlerResponse) {
+      return patternHandlerResponse;
+    }
   }
-  if (nextUrl.pathname === "/auth/signout") {
-    const response = NextResponse.redirect(new URL("/auth/signin", req.url))
-    response.cookies.delete("codebility-auth")
-    return response
-  }
-  if (cookies.has("codebility-auth")) {
-    const result = await isAuthenticated(req)
-    if (!result) {
-      const response = NextResponse.redirect(new URL("/auth/signin", req.url))
-      response.cookies.delete("codebility-auth")
-      return response
-    }
-    const decoded: any = jwtDecode(cookies.get("codebility-auth")?.value as string)
-    if (decoded && decoded.userType.name === "APPLICANT") {
-      const response = NextResponse.redirect(new URL("/waiting", req.url))
-      return response
-    }
-    const applicantUrl = req.url.split("/")
-    if (applicantUrl[applicantUrl.length - 1] === "applicants" && decoded && decoded.userType.name !== "ADMIN") {
-      const response = NextResponse.redirect(new URL("/dashboard", req.url))
-      return response
-    }
-    const internsUrl = req.url.split("/")
-    if (internsUrl[internsUrl.length - 1] === "interns" && decoded && decoded.userType.name !== "ADMIN") {
-      const response = NextResponse.redirect(new URL("/dashboard", req.url))
-      return response
-    }
-    const inhouseUrl = req.url.split("/") 
-    if(inhouseUrl[inhouseUrl.length - 1]  === "in-house" && decoded && decoded.userType.name !== "ADMIN"){
-      const response = NextResponse.redirect(new URL("/dashboard", req.url))
-      return response
-    }
-  } */
+
+  return res;
 }
 
 /**
@@ -93,8 +50,8 @@ export async function middleware(req: NextRequest) {
 
 function getPatterns() {
   return [
-    {
-      pattern: new URLPattern({ pathname: "/authv2/*?" }),
+    {   
+      pattern: new URLPattern({ pathname: `${pathsConfig.auth.signIn.split("/")[1]}/*?` }), // get sign in / up auth prefix.
       handler: async (req: NextRequest, res: NextResponse) => {
         const supabase = createMiddlewareClient({ req, res });
 
@@ -112,7 +69,7 @@ function getPatterns() {
         if (!user) return;
 
         // If user is logged in, redirect to their respective page.
-        let nextURL = new URL("/home", req.nextUrl.origin).href; // default for now, mentor.
+        let nextURL = new URL(pathsConfig.app.home, req.nextUrl.origin).href; // default for now, mentor.
         switch (userData?.application_status) {
           case "PENDING":
           case "DECLINED":
@@ -124,7 +81,7 @@ function getPatterns() {
       },
     },
     {
-      pattern: new URLPattern({ pathname: "/home/*?" }),
+      pattern: new URLPattern({ pathname: `${pathsConfig.app.home}/*?` }),
       handler: async (req: NextRequest, res: NextResponse) => {
         const {
           data: { user },
