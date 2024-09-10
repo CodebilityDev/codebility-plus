f4abfdea2a81a8068cbfddfa3ed5da8b60c5980e
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-09-10 10:47:48 +0800

    fix: use supabase server component client for server componnents.

--- apps/codebility/app/home/projects/page.tsx
+++ apps/codebility/app/home/projects/page.tsx
@@ -1,37 +1,31 @@
+import { use } from "react";
+import H1 from "@/Components/shared/dashboard/H1";
+import { ProjectT } from "@/types/index";
 
+import { getSupabaseServerComponentClient } from "@codevs/supabase/server-component-client";
 
-import ProjectCard from "./_components/ProjectCard"
-import { ProjectT } from "@/types/index"
-import H1 from "@/Components/shared/dashboard/H1"
-
-
-import InsertButton from "./_components/projects-insert-button"
-import { use } from "react"
-import { useSupabase } from "@codevs/supabase/hooks/use-supabase"
-
+import ProjectCard from "./_components/ProjectCard";
+import InsertButton from "./_components/projects-insert-button";
 
 const Projects = () => {
+  const supabase = getSupabaseServerComponentClient();
 
-
-  const supabase = useSupabase();
-
-
-  const Projects = use(supabase.from('projects').select('*').then(({ data, error }) => {
-    if (error) throw error;
-    return data;
-  }));
-
-
-
+  const Projects = use(
+    supabase
+      .from("projects")
+      .select("*")
+      .then(({ data, error }) => {
+        if (error) throw error;
+        return data;
+      }),
+  );
 
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-row justify-between gap-4">
         <H1>Projects</H1>
         <div className="flex items-center gap-4">
-
-          <InsertButton/>
-          
+          <InsertButton />
         </div>
       </div>
       {Projects && Projects.length > 0 && (
