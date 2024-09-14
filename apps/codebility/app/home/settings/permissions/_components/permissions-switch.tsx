32daf9276969e8db341b3343b44cae34477e62f8
Jedpalero <140801154+Jedpalero@users.noreply.github.com>
2024-09-14 12:02:26 +0800

    remove commented codes

--- apps/codebility/app/home/settings/permissions/_components/permissions-switch.tsx
+++ apps/codebility/app/home/settings/permissions/_components/permissions-switch.tsx
@@ -1,31 +1,34 @@
-"use client"
+"use client";
 
-import ToggleSwitch from '@/Components/ui/switch'
-import { useState } from 'react'
-import { SwitchProps } from '../_types/switch-props'
-import { updateBooleanField } from '../action'
+import { useState } from "react";
+import ToggleSwitch from "@/Components/ui/switch";
+
+import { SwitchProps } from "../_types/switch-props";
+import { updateBooleanField } from "../action";
 
 const Switch = ({ enabled, className, id, name }: SwitchProps) => {
-  const [isEnabled, setIsEnabled] = useState(enabled)
+  const [isEnabled, setIsEnabled] = useState(enabled);
 
   const handleClick = async () => {
-    const newState = !isEnabled
-    setIsEnabled(newState)
+    const newState = !isEnabled;
+    setIsEnabled(newState);
 
     try {
-      await updateBooleanField(id, name, newState)
-      console.log('Field updated successfully')
+      await updateBooleanField(id, name, newState);
+      console.log("Field updated successfully");
     } catch (error) {
-      console.error('Error updating field:', error)
-      setIsEnabled(enabled) // Revert to original state on error
+      console.error("Error updating field:", error);
+      setIsEnabled(enabled);
     }
-  }
+  };
 
   return (
-    <div className={`items-center justify-center md:flex md:p-2 lg:p-4 ${className}`}>
-         <ToggleSwitch onClick={handleClick} enabled={!isEnabled} />
-       </div>
-  )
-}
+    <div
+      className={`items-center justify-center md:flex md:p-2 lg:p-4 ${className}`}
+    >
+      <ToggleSwitch onClick={handleClick} enabled={!isEnabled} />
+    </div>
+  );
+};
 
-export default Switch
+export default Switch;
