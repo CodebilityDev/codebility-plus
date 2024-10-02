c04de7ab2b4b3564dbf12800b257e675a3e7fea3
ciscutiez <francischarlesi.gonzales@gmail.com>
2024-10-02 13:49:36 +0800

    fix: fix admin image

--- apps/codebility/app/(marketing)/_components/landing/landing-admin-card.tsx
+++ apps/codebility/app/(marketing)/_components/landing/landing-admin-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { index_AdminCardT } from "@/types/home";
 
 const AdminCard = ({ admin, color }: index_AdminCardT) => {
+
   return (
     <div>
       <div
@@ -12,7 +13,7 @@ const AdminCard = ({ admin, color }: index_AdminCardT) => {
           alt={`${admin.first_name} Avatar`}
           src={
             admin.image_url
-              ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/${admin.image_url}`
+              ? `${admin.image_url}`
               : "/assets/svgs/icon-codebility-black.svg"
           }
           width={60}
@@ -20,7 +21,7 @@ const AdminCard = ({ admin, color }: index_AdminCardT) => {
           className={`${color} h-[250px] w-full  rounded-lg bg-cover object-cover`}
         />
         <div className="flex w-full flex-col gap-1">
-          <p className="md:text-md text-sm text-white lg:text-lg">
+          <p className="md:text-md text-sm text-white lg:text-lg capitalize">
             {admin.first_name} {admin.last_name}
           </p>
           {admin.main_position ? (
