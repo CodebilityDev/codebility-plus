fd55ceb3bce943c0afe1c54fca85d87461111704
ciscutiez <francischarlesi.gonzales@gmail.com>
2024-10-07 17:02:14 +0800

    fix: combined the profile and interns table

--- apps/codebility/app/home/interns/page.tsx
+++ apps/codebility/app/home/interns/page.tsx
@@ -6,11 +6,31 @@ import InternContainer from "./_components/intern-container";
 
 const Interns = async () => {
   const supabase = getSupabaseServerComponentClient();
-  const { data: interns, error } = await supabase.from("interns").select("*");
+  const [
+    { data: interns, error: internsError },
+    { data: profiles, error: profilesError },
+  ] = await Promise.all([
+    supabase.from("interns").select("*"),
+    supabase.from("profile").select("user_id, main_position, image_url"),
+  ]);
+
+  if (internsError || profilesError) {
+    return "Error fetching data";
+  }
+
+  const data = interns.map((intern) => {
+    const profile = profiles.find((p) => p?.user_id === intern?.user_id);
+
+    return {
+      ...intern,
+      main_position: profile?.main_position || null,
+      image_url: profile?.image_url || null,
+    };
+  });
 
   return (
     <>
-      {error ? "error" : <InternContainer data={(interns as User[]) || []} />}
+      <InternContainer data={(data as User[]) || []} />
     </>
   );
 };
