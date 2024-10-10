b9b2de0c35a0f8efb021719cb0ec771ebf3a25c2
nerwinalamas <nerwinalamas@gmail.com>
2024-10-10 15:21:03 +0800

    refactor: similar to project status enum in supabase

--- apps/codebility/app/home/projects/_lib/constants.ts
+++ apps/codebility/app/home/projects/_lib/constants.ts
@@ -1,5 +1,5 @@
 const DEFAULT_AVATAR =
   "https://codebility-cdn.pages.dev/assets/images/default-avatar-200x200.jpg";
-const STATUS = ["Pending", "On-going", "Completed"];
+const STATUS = ["PENDING", "INPROGRESS", "COMPLETED"];
 
 export { DEFAULT_AVATAR, STATUS };
