0d11bad0b750194b28ee5a733acbea0b48f2b00a
nerwinalamas <nerwinalamas@gmail.com>
2024-10-10 13:21:27 +0800

    refactor: task view modal

--- apps/codebility/Components/modals/TaskViewModal.tsx
+++ apps/codebility/Components/modals/TaskViewModal.tsx
@@ -1,66 +1,211 @@
 "use client";
 
-import RenderTeam from "@/Components/shared/dashboard/RenderTeam";
-import { Paragraph } from "@/Components/shared/home";
+import Image from "next/image";
+import { handleCopy } from "@/app/home/kanban/[id]/_lib";
+import { DEFAULT_AVATAR } from "@/app/home/kanban/[id]/_lib/constants";
 import { Button } from "@/Components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+} from "@/Components/ui/select";
 import { useModal } from "@/hooks/use-modal";
-import { modals_TaskViewModal } from "@/types/components";
-import { TaskT } from "@/types/index";
+import { IconCopy } from "@/public/assets/svgs";
+import { Ellipsis } from "lucide-react";
+import toast from "react-hot-toast";
 
-import { Dialog, DialogContent } from "@codevs/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@codevs/ui/dropdown-menu";
+import { Input } from "@codevs/ui/input";
+import { Textarea } from "@codevs/ui/textarea";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
 
 const TaskViewModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
-
+  const { isOpen, onOpen, onClose, type, data } = useModal();
   const isModalOpen = isOpen && type === "taskViewModal";
 
-  const isTask = (
-    data: TaskT | modals_TaskViewModal[] | undefined,
-  ): data is TaskT => {
-    return (data as TaskT)?.title !== undefined;
+  const handleClose = () => {
+    onClose();
   };
 
   return (
-    <Dialog open={isModalOpen} onOpenChange={() => onClose()}>
-      <DialogContent className="h-[32rem] w-[80%] max-w-sm overflow-x-auto overflow-y-auto lg:h-auto">
-        <div className="flex flex-col gap-6">
-          {isTask(data) && (
-            <div key={data.id}>
-              <div className="relative flex flex-col gap-1">
-                <p className="text-lg font-semibold">{data.title}</p>
-                <p className="text-sm">{data.subheader}</p>
-                <Paragraph>{data.full_description}</Paragraph>
-                <div className="text-gray mt-1 flex items-center gap-2 text-xs">
-                  Duration Hours:{" "}
-                  <p className="text-dark-100 text-sm uppercase dark:text-white">
-                    {data.duration}
-                  </p>
-                </div>
-                <div className="text-gray mt-1 flex items-center gap-2 text-xs">
-                  Points:{" "}
-                  <p className="text-dark-100 text-sm uppercase dark:text-white">
-                    {data.task_points}
-                  </p>
-                </div>
-                <p className="text-sm">Priority Level: {data.prio_level}</p>
-              </div>
-
-              <div className="flex flex-row justify-between gap-2">
-                <div className="flex flex-col gap-1">
-                  <p className="text-sm">Members</p>
-                  <div className="grid grid-cols-3 gap-1 ">
-                    {data.userTask.map((userImage) => (
-                      <RenderTeam
-                        key={userImage.id}
-                        imgURL={userImage.image_url}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="h-[32rem] w-[90%] max-w-3xl overflow-y-auto lg:h-[44rem]">
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-4">
+            <DialogHeader className="relative">
+              <DialogTitle className="text-left text-lg font-bold capitalize">
+                {data?.title}
+              </DialogTitle>
+            </DialogHeader>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Ellipsis className="cursor-pointer" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start" className="dark:bg-black-200 w-56">
+                <DropdownMenuItem
+                  onClick={() => onOpen("taskEditModal", data)}
+                  className="cursor-pointer"
+                >
+                  <span>Edit</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => onOpen("taskDeleteModal", data)}
+                  className="cursor-pointer"
+                >
+                  <span>Delete</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              id="title"
+              type="text"
+              label="Task Name"
+              name="title"
+              placeholder="Enter Task Name"
+              className="dark:bg-dark-200 cursor-not-allowed"
+              value={data?.title}
+              disabled
+            />
+            <div className="flex flex-col justify-end">
+              <label htmlFor="category">Category</label>
+              <Select name="category" disabled>
+                <SelectTrigger>
+                  <SelectValue placeholder={data?.category}></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
-          )}
-          <Button variant="default">Pin</Button>
+            <Input
+              id="duration"
+              type="number"
+              label="Task Duration (in hrs)"
+              name="duration"
+              min="0"
+              step="0.25"
+              placeholder="Enter Task Duration"
+              className="dark:bg-dark-200 cursor-not-allowed"
+              value={data?.duration}
+              disabled
+            />
+            <Input
+              id="points"
+              type="number"
+              label="Points"
+              name="points"
+              min="0"
+              placeholder="Enter Task Points"
+              className="dark:bg-dark-200 cursor-not-allowed"
+              value={data?.points}
+              disabled
+            />
+            <div>
+              <label htmlFor="priority">Priority Level</label>
+              <Select name="priority" disabled>
+                <SelectTrigger>
+                  <SelectValue placeholder={data?.priority_level}></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <label htmlFor="type">Type</label>
+              <Select name="type" disabled>
+                <SelectTrigger>
+                  <SelectValue placeholder={data?.type}></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col gap-2">
+              <Input
+                id="branchName"
+                type="text"
+                label="Branch Name"
+                name="branchName"
+                className="dark:bg-dark-200 cursor-not-allowed"
+                value={data?.pr_link}
+                disabled
+              />
+              <button type="button" onClick={() => handleCopy(data.pr_link)}>
+                <IconCopy className="h-5 invert dark:invert-0" />
+              </button>
+            </div>
+            <Input
+              id="pr_link"
+              type="text"
+              label="Pull Request Link"
+              name="pr_link"
+              className="dark:bg-dark-200 cursor-not-allowed"
+              value={data?.pr_link}
+              disabled
+            />
+          </div>
+          <div className="flex flex-wrap items-center">
+            {data?.codev_task &&
+              data.codev_task.length > 0 &&
+              data?.codev_task.map((codev: any) => {
+                return (
+                  <div
+                    className="relative h-12 w-12 cursor-pointer rounded-full bg-cover object-cover"
+                    key={codev.id}
+                  >
+                    <Image
+                      alt="Avatar"
+                      src={codev.codev.user.profile.image_url || DEFAULT_AVATAR}
+                      fill
+                      title={`${codev.codev.first_name} ${codev.codev.last_name}'s avatar`}
+                      className="h-auto w-full rounded-full bg-cover object-cover"
+                      loading="eager"
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          <div>
+            <label htmlFor="desc">Description</label>
+            <Textarea
+              id="desc"
+              variant="ghost"
+              name="description"
+              className="dark:bg-dark-200 h-[8rem] cursor-not-allowed resize-none text-sm"
+              placeholder="Add a more detailed description..."
+              value={data?.description}
+              disabled
+            />
+          </div>
+
+          <DialogFooter className="flex flex-col gap-2 lg:flex-row">
+            <Button
+              type="button"
+              variant="hollow"
+              className="order-2 w-full sm:order-1 sm:w-[130px]"
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </DialogFooter>
         </div>
       </DialogContent>
     </Dialog>
