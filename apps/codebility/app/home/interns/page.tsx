6ef1d15cca9c109bc693fd3b41e30abc74b15f20
nerwinalamas <nerwinalamas@gmail.com>
2024-10-11 14:48:45 +0800

    refactor: interns page

--- apps/codebility/app/home/interns/page.tsx
+++ apps/codebility/app/home/interns/page.tsx
@@ -1,7 +1,5 @@
-import { User } from "@/types";
-
+import { Codev } from "@/types/home/codev";
 import { getSupabaseServerComponentClient } from "@codevs/supabase/server-component-client";
-
 import InternContainer from "./_components/intern-container";
 
 const Interns = async () => {
@@ -11,7 +9,7 @@ const Interns = async () => {
     { data: profiles, error: profilesError },
   ] = await Promise.all([
     supabase.from("interns").select("*"),
-    supabase.from("profile").select("user_id, main_position, image_url"),
+    supabase.from("profile").select("*, user(*, codev(*), social(*))"),
   ]);
 
   if (internsError || profilesError) {
@@ -22,15 +20,30 @@ const Interns = async () => {
     const profile = profiles.find((p) => p?.user_id === intern?.user_id);
 
     return {
-      ...intern,
-      main_position: profile?.main_position || null,
-      image_url: profile?.image_url || null,
+      id: intern.id,
+      email: intern.email,
+      user_id: intern.user_id,
+      first_name: intern.first_name,
+      last_name: intern.last_name,
+      image_url: profile.image_url,
+      address: profile.address,
+      about: profile.about,
+      contact: profile.user.social.phone_no,
+      education: profile.education,
+      socials: profile.user.social,
+      main_position: profile.main_position,
+      internal_status: profile.user.codev.internal_status,
+      tech_stacks: profile.tech_stacks,
+      nda_status: profile.user.codev.nda_status,
+      job_status: profile.user.codev.job_status,
+      portfolio_website: profile.portfolio_website,
+      type: profile.user.codev.type,
     };
   });
 
   return (
     <>
-      <InternContainer data={(data as User[]) || []} />
+      <InternContainer data={(data as Codev[]) || []} />
     </>
   );
 };
