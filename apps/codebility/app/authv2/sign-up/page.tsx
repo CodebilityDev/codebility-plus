a2030342251eb5f4d17350479430987d2dcde32c
nerwinalamas <nerwinalamas@gmail.com>
2024-10-12 19:51:25 +0800

    styles: update styling for sign up page

--- apps/codebility/app/authv2/sign-up/page.tsx
+++ apps/codebility/app/authv2/sign-up/page.tsx
@@ -6,8 +6,8 @@ import SignUpForm from "./_components/sign-up-form";
 
 const SignUp = () => {
   return (
-    <div className="bg-black-500 flex min-h-screen flex-col items-center justify-center px-3.5 text-white lg:flex-row">
-      <div className="flex w-full max-w-[60rem] flex-col justify-center gap-2 rounded-2xl bg-[#1E1F26] p-8">
+    <div className="bg-black-500 flex min-h-screen items-center justify-center text-white md:px-4 ">
+      <div className="flex w-full max-w-[60rem] flex-col justify-center gap-2 bg-[#1E1F26] py-8 md:rounded-2xl xl:p-8">
         <Link href="/" className="mx-auto mb-6">
           <Image
             className="h-[45px] w-[200px] cursor-pointer"
@@ -17,7 +17,7 @@ const SignUp = () => {
             alt="Codebility Logo"
           />
         </Link>
-        <p className=" text-center text-xl font-semibold">Create new account</p>
+        <p className="text-center text-xl font-semibold">Create new account</p>
         <p className="text-md text-center">
           To use Codebility, please enter your details
         </p>
