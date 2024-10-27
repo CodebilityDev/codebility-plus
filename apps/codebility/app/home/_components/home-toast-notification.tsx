fbccad012c817a5060e1b918fe829527ad966c1d
Apollo <apollomacabodbod8@gmail.com>
2024-10-27 11:25:09 +0800

    fix(home): added pathname

--- apps/codebility/app/home/_components/home-toast-notification.tsx
+++ apps/codebility/app/home/_components/home-toast-notification.tsx
@@ -1,9 +1,11 @@
 "use client"; // This makes the component a client component
-import { memo, useState, useEffect } from "react";
+import {  useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
+import { usePathname } from "next/navigation";
 
 const ToastNotification = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const pathname = usePathname();
 
   // Set isMounted to true on client mount
   useEffect(() => {
@@ -12,7 +14,7 @@ const ToastNotification = () => {
 
   // Show the toast only after the component has mounted
   useEffect(() => {
-    if (isMounted) {
+    if (isMounted && pathname === "/home") {
       toast.success("Congratulations! You are now an official member of Codebility.");
     }
   }, [isMounted]);
