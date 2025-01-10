6ddb3bc1bbf518ac7cf9bcdb0ed4c953ca2f94ed
adrian mores <adrian.tm@live.com>
2025-01-10 09:34:39 +0800

    fix: settings/resume: Save button is disabled until at least one field has changed values

--- apps/codebility/app/home/settings/resume/_components/resume-about.tsx
+++ apps/codebility/app/home/settings/resume/_components/resume-about.tsx
@@ -1,62 +1,61 @@
-"use client"
-import {  useEffect, useState } from "react"
-import toast from "react-hot-toast"
-import { useForm } from "react-hook-form"
-import { Button } from "@/Components/ui/button"
-import { IconEdit } from "@/public/assets/svgs"
-import Box from "@/Components/shared/dashboard/Box"
-import { Textarea } from "@codevs/ui/textarea"
-import { Label } from "@codevs/ui/label"
-import {   updateProfile } from "../action"
-import { Profile_Types } from "../_types/resume"
+"use client";
+
+import { watch } from "fs";
+import { useEffect, useState } from "react";
+import Box from "@/Components/shared/dashboard/Box";
+import { Button } from "@/Components/ui/button";
+import { IconEdit } from "@/public/assets/svgs";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+import { Label } from "@codevs/ui/label";
+import { Textarea } from "@codevs/ui/textarea";
+
+import { Profile_Types } from "../_types/resume";
+import { updateProfile } from "../action";
 
 type About_Props = {
-data: Profile_Types
-}
+  data: Profile_Types;
+};
 
-const About = ({data}: About_Props) => {
-  const [isEditMode, setIsEditMode] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+const About = ({ data }: About_Props) => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { about } = data;
 
   const {
     register,
     handleSubmit,
     reset,
-    formState: {},
-  } = useForm(
-    {
+    watch,
+    formState: { isDirty },
+  } = useForm({
     defaultValues: {
-        about: "",
-  },})
-  useEffect(() => {
-    if(data) {
-      reset({
-        about: data.about,
-      })
-    }
-  }, [data, reset])
+      about,
+    },
+  });
 
   const onSubmit = async (data: any) => {
-    const toastId = toast.loading("Your info was being updated")
+    const toastId = toast.loading("Your info is being updated");
     try {
-      setIsLoading(true)
-      const {about} = data
-      await updateProfile({about})
-      toast.success("Your about was sucessfully updated!", {id: toastId})
-      setIsEditMode(false)
-    } catch(error){
-      console.log(error)
-      toast.error("Something went wrong, Please try again later!")
+      setIsLoading(true);
+      await updateProfile(data);
+      toast.success("Your about was sucessfully updated!", { id: toastId });
+      setIsEditMode(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong, please try again later!");
     } finally {
       setIsLoading(false);
     }
   };
 
-  const handleEditClick = () => {
-    setIsEditMode(!isEditMode);
-  };
-
-  const handleSaveClick = () => {
+  const handleCancel = () => {
+    reset();
+    toast("Update cancelled", {
+      duration: 2000,
+      icon: "⚠️",
+    });
     setIsEditMode(false);
   };
 
@@ -68,7 +67,7 @@ const About = ({data}: About_Props) => {
             ? "hidden"
             : "w-15 h-15 absolute right-6 top-6 cursor-pointer invert dark:invert-0"
         } `}
-        onClick={handleEditClick}
+        onClick={() => setIsEditMode(true)}
       />
 
       <p className="text-lg">About</p>
@@ -85,6 +84,7 @@ const About = ({data}: About_Props) => {
                 id="about_me"
                 {...register("about")}
                 disabled={!isEditMode}
+                value={watch("about")}
                 className={` placeholder-${
                   !isEditMode
                     ? "lightgray dark:placeholder-gray"
@@ -102,12 +102,16 @@ const About = ({data}: About_Props) => {
             <div className="mt-4 flex justify-end gap-2">
               <Button
                 variant="hollow"
-                onClick={handleSaveClick}
+                onClick={handleCancel}
                 disabled={isLoading}
               >
                 Cancel
               </Button>
-              <Button variant="default" type="submit" disabled={isLoading}>
+              <Button
+                variant="default"
+                type="submit"
+                disabled={isLoading || !isDirty}
+              >
                 {isLoading ? "Saving..." : "Save"}
               </Button>
             </div>
@@ -118,4 +122,4 @@ const About = ({data}: About_Props) => {
   );
 };
 
-export default About
+export default About;
