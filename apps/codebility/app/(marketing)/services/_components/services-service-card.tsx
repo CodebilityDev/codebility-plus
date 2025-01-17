07216deb4451f6bde2b23ac964226804962fe02f
Adrian <adrian.tm@live.com>
2025-01-17 11:11:51 +0800

    fix: quick fix to maintain tab order, fix alignment of cards

--- apps/codebility/app/(marketing)/services/_components/services-service-card.tsx
+++ apps/codebility/app/(marketing)/services/_components/services-service-card.tsx
@@ -12,22 +12,24 @@ export default function ServiceCard({ service }: Props) {
   const isImageUrlSegmented = mainImage && mainImage.split("/")[0] === "public";
 
   return (
-    <div className="border-light-900/5 bg-light-700/10 flex flex-1 flex-col justify-between gap-4 rounded-lg border-2 p-4 text-white">
-      <Image
-        src={
-          mainImage && isImageUrlSegmented
-            ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/services-image/${mainImage}`
-            : mainImage && !isImageUrlSegmented
-              ? mainImage
-              : "https://codebility-cdn.pages.dev/assets/images/dafault-avatar-1248x845.jpg"
-        }
-        alt={name}
-        width={600}
-        height={600}
-        className="h-full w-full rounded-lg object-cover"
-        priority
-      />
-      <div className="flex h-1/3 flex-col gap-3">
+    <div className="border-light-900/5 bg-light-700/10 flex flex-1 flex-col gap-4 rounded-lg border-2 p-4 text-white">
+      <div className="relative aspect-video w-full overflow-hidden rounded-lg">
+        <Image
+          src={
+            mainImage && isImageUrlSegmented
+              ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/services-image/${mainImage}`
+              : mainImage && !isImageUrlSegmented
+                ? mainImage
+                : "https://codebility-cdn.pages.dev/assets/images/dafault-avatar-1248x845.jpg"
+          }
+          alt={name}
+          fill
+          sizes="600px"
+          className="absolute bg-center object-cover"
+          priority
+        />
+      </div>
+      <div className="flex basis-1/4 flex-col gap-3">
         <h3 className="line-clamp-1 text-base font-medium lg:text-xl">
           {name}
         </h3>
