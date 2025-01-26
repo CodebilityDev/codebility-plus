479690e36df4fa1838bdc525723ed8741201b2aa
zeff <jzeffsomera@gmail.com>
2025-01-26 21:52:37 +0800

    delete useless files

--- apps/codebility/types/index.d.ts
+++ apps/codebility/types/index.d.ts
@@ -1,71 +1,6 @@
 import React from "react";
 import { ExperienceType } from "@/app/home/settings/resume/experience";
 
-type TStatus =
-  | "Available"
-  | "Deployed"
-  | "Training"
-  | "Vacation"
-  | "Busy"
-  | "Client Ready"
-  | "Blocked"
-  | "Graduated";
-
-type TPosition =
-  | "Frontend Developer"
-  | "Backend Developer"
-  | "Fullstack Developer"
-  | "Mobile Developer"
-  | "UI/UX Designer"
-  | "Graphic Designer"
-  | "Social Media Manager"
-  | "Digital Marketer"
-  | "Human Resource"
-  | "Video Editor"
-  | "Project Manager"
-  | "Admin"
-  | "CEO";
-
-type TNda = "Received" | "Sent";
-
-type User = {
-  id: string;
-  first_name: string;
-  last_name: string;
-  about_me?: string;
-  image_icon?: string;
-  image_url?: string | null;
-
-  pronoun?: string | null;
-  address?: string;
-  email_address?: string | null;
-  umber?: string | null;
-  github_link?: string | null;
-  fb_link?: string | null;
-  linkedin_link?: string | null;
-  whatsapp_link?: string | null;
-  skype_link?: string | null;
-  telegram_link?: string | null;
-  portfolio_website?: string | null;
-  tech_stacks?: string[];
-  addtl_skills?: string[];
-  about_me?: string | null;
-  education?: string | null;
-  created_at?: string;
-  updated_at?: string;
-  schedule?: string | null;
-  position?: string[];
-  roleType?: string;
-  userType?: string;
-  projects?: any[];
-  clientId?: string;
-  main_position?: string;
-  start_time?: string | null;
-  end_time?: string | null;
-  jobStatusType?: string;
-  prio?: number;
-  Work_Experience?: ExperienceType[] | [];
-};
 
 type Tag = {
   tag: string;
@@ -77,19 +12,6 @@ type Sidebar = {
   links: Link[];
 };
 
-type Link = {
-  route: string;
-  imgURL: string;
-  label: string;
-  permission: string;
-};
-
-type SocialIcons = {
-  imgURL: string;
-  route: string;
-  label: string;
-};
-
 type topNotcher = {
   id: number;
   ranking: number;
@@ -98,47 +20,7 @@ type topNotcher = {
   level: number;
 };
 
-type TaskT = {
-  id: string;
-  title: string;
-  subheader: string;
-  full_description: string;
-  userTodoId: string | null;
-  projectId: string | null;
-  task_points: number;
-  prio_level: string;
-  github_link: string;
-  created_at: string;
-  duration: string;
-  listId: string;
-  created_at: Date;
-  updated_at: string;
-  task_category: string;
-  task_type: string;
-  pr_link: string;
-  tagId: string[];
-  tags: Tag[];
-  userTodo: any;
-  userTask: [
-    {
-      id: string;
-      first_name: string;
-      last_name: string;
-      image_url: string;
-    },
-  ];
-  projects: any;
-};
 
-type ListT = {
-  [x: string]: any;
-  id: string;
-  created_at: string;
-  updated_at: string;
-  name: string;
-  boardId: string;
-  task: [];
-};
 
 interface IInhouse {
   data: TeamMemberT[];
