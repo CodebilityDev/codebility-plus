08b996c03e7ec67b31118d74f737f90d6b338e51
arvee0220 <rowellverdejo@gmail.com>
2025-02-26 17:20:55 +0800

    Fix loading page not rendering when resources are being loaded

--- apps/codebility/app/home/applicants/page.tsx
+++ apps/codebility/app/home/applicants/page.tsx
@@ -1,32 +1,7 @@
-import ApplicantsList from "@/app/home/applicants/_components/ApplicantsList";
-import { getCodevs } from "@/lib/server/codev.service";
+import ApplicantsPageClient from "./_components/ApplicantsPageClient";
 
 const ApplicantsPage = async () => {
-  const { data: applicants, error } = await getCodevs({
-    filters: {
-      role_id: 7,
-      application_status: "applying",
-    },
-  });
-
-  // Handle error scenario
-  if (error) {
-    console.error("Error fetching applicants:", error);
-    return (
-      <div className="mx-auto flex max-w-screen-xl flex-col gap-4">
-        <div>ERROR: Unable to fetch applicants</div>
-      </div>
-    );
-  }
-
-  // Ensure applicants is always an array
-  const safeApplicants = applicants || [];
-
-  return (
-    <div className="mx-auto flex max-w-screen-xl flex-col gap-4 ">
-      <ApplicantsList applicants={safeApplicants} />
-    </div>
-  );
+  return <ApplicantsPageClient />;
 };
 
 export default ApplicantsPage;
