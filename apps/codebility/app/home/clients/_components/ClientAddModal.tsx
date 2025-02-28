08b0846ab9dfa0fd999866f0f54100ab3aaccf4d
arvee0220 <rowellverdejo@gmail.com>
2025-02-28 17:56:26 +0800

    Create and test new ClientAddModal component

--- apps/codebility/app/home/clients/_components/ClientAddModal.tsx
+++ apps/codebility/app/home/clients/_components/ClientAddModal.tsx
@@ -18,7 +18,81 @@ import { DialogTitle } from "@radix-ui/react-dialog";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@codevs/ui/form";
 import { Input } from "@codevs/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@codevs/ui/select";
+
+type FormItems = {
+  labelText: string;
+  placeHolderText: string;
+  inputType: string;
+  formDefaultValue: string;
+  options?: { value: string; label: string }[];
+};
+
+const formItemLabels: FormItems[] = [
+  {
+    labelText: "Name",
+    placeHolderText: "Enter Company Name",
+    inputType: "text",
+    formDefaultValue: "name",
+  },
+  {
+    labelText: "Email",
+    placeHolderText: "Enter Company Email Address",
+    inputType: "email",
+    formDefaultValue: "email",
+  },
+  {
+    labelText: "Address",
+    placeHolderText: "Enter Company Address",
+    inputType: "text",
+    formDefaultValue: "address",
+  },
+  {
+    labelText: "Client Type",
+    placeHolderText: "Select client type",
+    inputType: "",
+    formDefaultValue: "organization",
+    options: [
+      { value: "individual", label: "Individual" },
+      { value: "organization", label: "Organization" },
+    ],
+  },
+  {
+    labelText: "Phone Number",
+    placeHolderText: "Enter Company Phone Number",
+    inputType: "tel",
+    formDefaultValue: "phone_number",
+  },
+  {
+    labelText: "Website",
+    placeHolderText: "https://example.com",
+    inputType: "url",
+    formDefaultValue: "website",
+  },
+  {
+    labelText: "Country",
+    placeHolderText: "Select a country",
+    inputType: "",
+    formDefaultValue: "country",
+    options: [{ value: "philippines", label: "Philippines" }],
+  },
+];
 
 export default function ClientAddModal() {
   const { isOpen, onClose, type } = useModal();
@@ -27,20 +101,15 @@ export default function ClientAddModal() {
   const [isLoading, setIsLoading] = useState(false);
   const [logoPreview, setLogoPreview] = useState<string | null>(null);
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    setValue,
-    formState: { errors, isValid },
-  } = useForm<ClientFormValues>({
+  const form = useForm<ClientFormValues>({
     resolver: zodResolver(clientSchema),
+    defaultValues: {},
     mode: "onChange",
   });
 
   const handleDialogChange = (open: boolean) => {
     if (!open) {
-      reset();
+      form.reset();
       setLogoPreview(null);
     }
     onClose();
@@ -49,14 +118,14 @@ export default function ClientAddModal() {
   const handleLogoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      setValue("company_logo", file); // store as `company_logo`
+      form.setValue("company_logo", file); // store as `company_logo`
       setLogoPreview(URL.createObjectURL(file));
     }
   };
 
   const handleRemoveLogo = () => {
     setLogoPreview(null);
-    setValue("company_logo", undefined);
+    form.setValue("company_logo", undefined);
   };
 
   const handleCreateClient = async (data: ClientFormValues) => {
@@ -104,166 +173,141 @@ export default function ClientAddModal() {
           <DialogTitle className="text-2xl">Add New Company</DialogTitle>
         </DialogHeader>
 
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-center md:justify-start">
-            <label className="md:text-md text-sm lg:text-lg">Logo</label>
-          </div>
-          <div className="flex flex-col gap-4 md:flex-row">
-            <div className="relative mx-auto flex size-[100px] md:mx-0 md:size-[80px]">
-              {logoPreview ? (
-                <Image
-                  src={logoPreview}
-                  alt="Company Logo"
-                  fill
-                  className="bg-dark-400 h-auto w-auto rounded-full bg-cover object-cover"
-                />
-              ) : (
-                <DefaultAvatar />
-              )}
-            </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleCreateClient)}>
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-center md:justify-start">
+                <label className="md:text-md text-sm lg:text-lg">Logo</label>
+              </div>
+              <div className="flex flex-col gap-4 md:flex-row">
+                <div className="relative mx-auto flex size-[100px] md:mx-0 md:size-[80px]">
+                  {logoPreview ? (
+                    <Image
+                      src={logoPreview}
+                      alt="Company Logo"
+                      fill
+                      className="bg-dark-400 h-auto w-auto rounded-full bg-cover object-cover"
+                    />
+                  ) : (
+                    <DefaultAvatar />
+                  )}
+                </div>
 
-            <div className="flex flex-col justify-center gap-2">
-              <p className="text-md text-gray text-center">
-                Image size 1080 x 768 px
-              </p>
-              <div className="gap-4">
-                {!logoPreview && (
-                  <label htmlFor="company_logo">
-                    <p className="cursor-pointer text-center text-blue-100 md:text-left">
-                      Upload Image
-                    </p>
-                  </label>
-                )}
-                <input
-                  id="company_logo"
-                  type="file"
-                  accept="image/*"
-                  className="hidden"
-                  name="company_logo"
-                  onChange={handleLogoChange}
-                />
-                {logoPreview && (
-                  <p
-                    onClick={handleRemoveLogo}
-                    className="text-violet cursor-pointer text-center md:text-left"
-                  >
-                    Remove Image
+                <div className="flex flex-col justify-center gap-2">
+                  <p className="text-md text-gray text-center">
+                    Image size 1080 x 768 px
                   </p>
-                )}
+                  <div className="gap-4">
+                    {!logoPreview && (
+                      <label htmlFor="company_logo">
+                        <p className="cursor-pointer text-center text-blue-100 md:text-left">
+                          Upload Image
+                        </p>
+                      </label>
+                    )}
+                    <input
+                      id="company_logo"
+                      type="file"
+                      accept="image/*"
+                      className="hidden"
+                      name="company_logo"
+                      onChange={handleLogoChange}
+                    />
+                    {logoPreview && (
+                      <p
+                        onClick={handleRemoveLogo}
+                        className="text-violet cursor-pointer text-center md:text-left"
+                      >
+                        Remove Image
+                      </p>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+            {formItemLabels.map(
+              (
+                {
+                  labelText,
+                  placeHolderText,
+                  inputType,
+                  formDefaultValue,
+                  options,
+                },
+                idx,
+              ) => (
+                <FormField
+                  key={idx}
+                  control={form.control}
+                  name={formDefaultValue as keyof ClientFormValues}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{labelText}</FormLabel>
+                      <FormControl>
+                        {inputType === "file" ? (
+                          <Input
+                            type="file"
+                            accept="image/*"
+                            onChange={(e) =>
+                              field.onChange(e.target.files?.[0] || null)
+                            }
+                          />
+                        ) : options ? (
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={String(field.value)}
+                          >
+                            <SelectTrigger className="bg-background">
+                              <SelectValue placeholder={placeHolderText} />
+                            </SelectTrigger>
+                            <SelectContent>
+                              {options.map(({ value, label }) => (
+                                <SelectItem key={value} value={value}>
+                                  {label}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        ) : (
+                          <Input
+                            placeholder={placeHolderText}
+                            type={inputType}
+                            {...field}
+                            value={field.value as string}
+                            className={
+                              form.formState.errors.name
+                                ? "border border-red-500 focus:outline-none"
+                                : ""
+                            }
+                          />
+                        )}
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              ),
+            )}
 
-        <form onSubmit={handleSubmit(handleCreateClient)}>
-          <div className="flex flex-col gap-8 lg:flex-row">
-            <div className="flex flex-1 flex-col gap-4">
-              <Input
-                variant="lightgray"
-                label="Name"
-                placeholder="Enter Company Name"
-                {...register("name")}
-                className={
-                  errors.name ? "border border-red-500 focus:outline-none" : ""
-                }
-              />
-              {errors.name && (
-                <span className="text-sm text-red-400">
-                  {errors.name.message}
-                </span>
-              )}
-
-              <Input
-                variant="lightgray"
-                label="Email"
-                placeholder="Enter Company Email Address"
-                type="email"
-                {...register("email")}
-                className={
-                  errors.email ? "border border-red-500 focus:outline-none" : ""
-                }
-              />
-              {errors.email && (
-                <span className="text-sm text-red-400">
-                  {errors.email.message}
-                </span>
-              )}
-
-              <Input
-                variant="lightgray"
-                label="Address"
-                placeholder="Enter Company Address"
-                {...register("address")}
-                className={
-                  errors.address
-                    ? "border border-red-500 focus:outline-none"
-                    : ""
-                }
-              />
-              {errors.address && (
-                <span className="text-sm text-red-400">
-                  {errors.address.message}
-                </span>
-              )}
-            </div>
-
-            <div className="flex flex-1 flex-col gap-4">
-              <Input
-                variant="lightgray"
-                label="Phone Number"
-                placeholder="Enter Company Phone Number"
-                type="tel"
-                {...register("phone_number")}
-                className={
-                  errors.phone_number
-                    ? "border border-red-500 focus:outline-none"
-                    : ""
-                }
-              />
-              {errors.phone_number && (
-                <span className="text-sm text-red-400">
-                  {errors.phone_number.message}
-                </span>
-              )}
-
-              <Input
-                variant="lightgray"
-                label="Website"
-                placeholder="https://example.com"
-                {...register("website")}
-                className={
-                  errors.website
-                    ? "border border-red-500 focus:outline-none"
-                    : ""
-                }
-              />
-              {errors.website && (
-                <span className="text-sm text-red-400">
-                  {errors.website.message}
-                </span>
-              )}
-            </div>
-          </div>
-
-          <DialogFooter className="mt-8 flex flex-col gap-2 lg:flex-row">
-            <Button
-              type="button"
-              variant="hollow"
-              className="order-2 w-full sm:order-1 sm:w-[130px]"
-              onClick={() => handleDialogChange(false)}
-              disabled={isLoading}
-            >
-              Cancel
-            </Button>
-            <Button
-              type="submit"
-              className="order-1 w-full sm:order-2 sm:w-[130px]"
-              disabled={isLoading || !isValid}
-            >
-              Save
-            </Button>
-          </DialogFooter>
-        </form>
+            <DialogFooter className="mt-8 flex flex-col gap-2 lg:flex-row">
+              <Button
+                type="button"
+                variant="hollow"
+                className="order-2 w-full sm:order-1 sm:w-[130px]"
+                onClick={() => handleDialogChange(false)}
+                disabled={isLoading}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                className="order-1 w-full sm:order-2 sm:w-[130px]"
+                disabled={isLoading || !form.formState.isValid}
+              >
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
