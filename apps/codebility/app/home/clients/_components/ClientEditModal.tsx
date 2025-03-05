fe69f1b4a256daa4d97de080d2b6176133995bd2
arvee0220 <rowellverdejo@gmail.com>
2025-03-05 21:27:22 +0800

    Update action.ts updateClientAction

--- apps/codebility/app/home/clients/_components/ClientEditModal.tsx
+++ apps/codebility/app/home/clients/_components/ClientEditModal.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 // Import your types + form schema
 import {
-  ClientFormValues,
   clientSchema,
   ClientWithStatusFormValues,
   getFormItemLabels,
@@ -24,6 +23,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@codevs/ui/form";
 import { Input } from "@codevs/ui/input";
 import {
   Select,
@@ -77,12 +83,19 @@ export default function ClientEditModal() {
       country: "",
       phone_number: "",
     },
-    mode: "onBlur",
+    mode: "onChange",
   });
 
-  const { register, handleSubmit, reset, setValue, watch, formState } = form;
+  const {
+    handleSubmit,
+    reset,
+    setValue,
+    watch,
+    formState: { errors, isValid },
+  } = form;
 
-  const { errors, isValid } = formState;
+  console.log("IsValid: ", isValid);
+  console.log("errors: ", errors);
 
   // We'll watch the `status` field to show on the UI
   const currentStatus = watch("status");
@@ -167,6 +180,7 @@ export default function ClientEditModal() {
     }
 
     setIsLoading(true);
+
     try {
       // We'll send everything as FormData to your existing action
       const dataToSend = new FormData();
@@ -244,7 +258,7 @@ export default function ClientEditModal() {
                     </p>
                   </label>
                 )}
-                <input
+                <Input
                   id="edit_company_logo"
                   type="file"
                   accept="image/*"
@@ -299,116 +313,104 @@ export default function ClientEditModal() {
         </div>
 
         {/* MAIN FORM FIELDS */}
-        <form onSubmit={handleSubmit(handleUpdateClient)}>
-          <div className="flex flex-col gap-8 lg:flex-row">
-            <div className="flex flex-1 flex-col gap-4">
-              <Input
-                variant="lightgray"
-                label="Name"
-                placeholder="Enter Company Name"
-                {...register("name")}
-                className={
-                  errors.name ? "border border-red-500 focus:outline-none" : ""
-                }
-              />
-              {errors.name && (
-                <span className="text-sm text-red-400">
-                  {errors.name.message}
-                </span>
-              )}
-
-              <Input
-                variant="lightgray"
-                label="Email"
-                placeholder="Enter Company Email Address"
-                type="email"
-                {...register("email")}
-                className={
-                  errors.email ? "border border-red-500 focus:outline-none" : ""
-                }
-              />
-              {errors.email && (
-                <span className="text-sm text-red-400">
-                  {errors.email.message}
-                </span>
-              )}
-
-              <Input
-                variant="lightgray"
-                label="Address"
-                placeholder="Enter Company Address"
-                {...register("address")}
-                className={
-                  errors.address
-                    ? "border border-red-500 focus:outline-none"
-                    : ""
-                }
-              />
-              {errors.address && (
-                <span className="text-sm text-red-400">
-                  {errors.address.message}
-                </span>
-              )}
-            </div>
-
-            <div className="flex flex-1 flex-col gap-4">
-              <Input
-                variant="lightgray"
-                label="Phone Number"
-                placeholder="Enter Company Phone Number"
-                type="tel"
-                {...register("phone_number")}
-                className={
-                  errors.phone_number
-                    ? "border border-red-500 focus:outline-none"
-                    : ""
-                }
-              />
-              {errors.phone_number && (
-                <span className="text-sm text-red-400">
-                  {errors.phone_number.message}
-                </span>
-              )}
-
-              <Input
-                variant="lightgray"
-                label="Website"
-                placeholder="https://example.com"
-                {...register("website")}
-                className={
-                  errors.website
-                    ? "border border-red-500 focus:outline-none"
-                    : ""
-                }
-              />
-              {errors.website && (
-                <span className="text-sm text-red-400">
-                  {errors.website.message}
-                </span>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(handleUpdateClient)}>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+              {formFields.map(
+                (
+                  {
+                    labelText,
+                    placeHolderText,
+                    inputType,
+                    formDefaultValue,
+                    options,
+                  },
+                  idx,
+                ) => (
+                  <FormField
+                    key={idx}
+                    control={form.control}
+                    name={formDefaultValue as keyof ClientWithStatusFormValues}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{labelText}</FormLabel>
+                        <FormControl>
+                          {options ? (
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={String(field.value) ?? ""}
+                            >
+                              <SelectTrigger className="focus:ring-inherit">
+                                <SelectValue placeholder={placeHolderText} />
+                              </SelectTrigger>
+                              <SelectContent>
+                                {options.map((option) => (
+                                  <SelectItem
+                                    key={option.value}
+                                    value={option.value}
+                                  >
+                                    {option.label}
+                                  </SelectItem>
+                                ))}
+                              </SelectContent>
+                            </Select>
+                          ) : (
+                            <Input
+                              type={inputType || "text"}
+                              placeholder={placeHolderText}
+                              {...field}
+                              className={`${
+                                errors[
+                                  formDefaultValue as keyof ClientWithStatusFormValues
+                                ]
+                                  ? "border border-red-500 focus:outline-none"
+                                  : ""
+                              }`}
+                              value={field.value as string}
+                              onBlur={(e) => {
+                                const value = e.target.value
+                                  .trim()
+                                  .toLowerCase();
+                                if (
+                                  formDefaultValue === "website" &&
+                                  value &&
+                                  !value.startsWith("http://") &&
+                                  !value.startsWith("https://")
+                                ) {
+                                  field.onChange(`https://${value}`);
+                                }
+                              }}
+                            />
+                          )}
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                ),
               )}
             </div>
-          </div>
 
-          {/* FOOTER BUTTONS */}
-          <DialogFooter className="mt-8 flex flex-col gap-2 lg:flex-row">
-            <Button
-              type="button"
-              variant="hollow"
-              className="order-2 w-full sm:order-1 sm:w-[130px]"
-              onClick={() => handleDialogChange(false)}
-              disabled={isLoading}
-            >
-              Cancel
-            </Button>
-            <Button
-              type="submit"
-              className="order-1 w-full sm:order-2 sm:w-[130px]"
-              disabled={isLoading || !isValid}
-            >
-              Save
-            </Button>
-          </DialogFooter>
-        </form>
+            {/* FOOTER BUTTONS */}
+            <DialogFooter className="mt-8 flex flex-col gap-2 lg:flex-row">
+              <Button
+                type="button"
+                variant="hollow"
+                className="order-2 w-full sm:order-1 sm:w-[130px]"
+                onClick={() => handleDialogChange(false)}
+                disabled={isLoading}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                className="order-1 w-full sm:order-2 sm:w-[130px]"
+                disabled={isLoading || !isValid}
+              >
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
