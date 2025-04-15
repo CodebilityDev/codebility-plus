ef5491efe85a9352bfd128987db64eab096ac00c
Ruru232 <sanchez.rolando1998@yahoo.com>
2025-04-15 16:02:59 +0800

    feat: Added roadmap

--- apps/codebility/app/home/layout.tsx
+++ apps/codebility/app/home/layout.tsx
@@ -6,10 +6,12 @@ import { ThemeProvider } from "@/context/ThemeProvider";
 import ReactQueryProvider from "@/hooks/reactQuery";
 import { useUserStore } from "@/store/codev-store";
 import { UserProvider } from "@/store/UserProvider";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 
 import ToastNotification from "./_components/HomeToastNotification";
 import LeftSidebar from "./_components/LeftSidebar";
 import Navbar from "./_components/Navbar";
+import { MuiStyleRoot } from "./(dashboard)/_components/DashboardRoadmapStyleRoot";
 
 export default function HomeLayout({
   children,
@@ -28,17 +30,21 @@ export default function HomeLayout({
         <ThemeProvider>
           <ModalProviderHome />
           <ToastNotification />
-          <main className="background-light850_dark100 relative">
-            <Navbar />
-            <div className="flex">
-              <LeftSidebar />
-              <section className="background-lightsection_darksection flex min-h-screen flex-1 flex-col px-4 pb-6 pt-28 max-md:pb-14 md:px-8 lg:px-12">
-                <div className="max-w-8xl mx-auto h-full w-full">
-                  {children}
+          <AppRouterCacheProvider>
+            <MuiStyleRoot>
+              <main className="background-light850_dark100 relative">
+                <Navbar />
+                <div className="flex">
+                  <LeftSidebar />
+                  <section className="background-lightsection_darksection flex min-h-screen flex-1 flex-col px-4 pb-6 pt-28 max-md:pb-14 md:px-8 lg:px-12">
+                    <div className="max-w-8xl mx-auto h-full w-full">
+                      {children}
+                    </div>
+                  </section>
                 </div>
-              </section>
-            </div>
-          </main>
+              </main>
+            </MuiStyleRoot>
+          </AppRouterCacheProvider>
         </ThemeProvider>
       </UserProvider>
     </ReactQueryProvider>
