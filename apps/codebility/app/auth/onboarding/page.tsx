13a0fc8191f2a812260e384b64a75cdc8c35c463
Marco Lazaro <lazaromarco1526@gmail.com>
2025-04-29 13:15:17 +0800

    new update

--- apps/codebility/app/auth/onboarding/page.tsx
+++ apps/codebility/app/auth/onboarding/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import React, { useState } from 'react'
-import Onboarding1 from './_components/Onboarding1'
-import Onboarding2 from './_components/Onboarding2'
-import Onboarding3 from './_components/Onboarding3'
-import Onboarding4 from './_components/Onboarding4'
-import { Button, Stepper, Step } from '@material-tailwind/react'
-import Onboarding5 from './_components/Onboarding5';
-import Onboarding6 from './_components/Onboarding6';
-import Onboarding7 from './_components/Onboarding7';
+import React, { useState } from "react";
+import { Button, Step, Stepper } from "@material-tailwind/react";
+
+import Onboarding1 from "./_components/Onboarding1";
+import Onboarding2 from "./_components/Onboarding2";
+import Onboarding3 from "./_components/Onboarding3";
+import Onboarding4 from "./_components/Onboarding4";
+import Onboarding5 from "./_components/Onboarding5";
+import Onboarding6 from "./_components/Onboarding6";
+import Onboarding7 from "./_components/Onboarding7";
+
 
 
 const OnboardingPage = () => {
@@ -36,95 +38,119 @@ const OnboardingPage = () => {
   };
 
   return (
-    <div className="w-full h-screen">
-      <div className="absolute bottom-0 left-0 w-full z-20 p-4">
+    <div className="h-screen w-full">
+      <div className="absolute bottom-0 left-0 z-20 w-full p-4">
         <div className="w-full px-8 py-4">
           <Stepper
-             activeStep={activeStep}
-             isLastStep={(value) => setIsLastStep(value)}
-             isFirstStep={(value) => setIsFirstStep(value)}
-             lineClassName="bg-white/30 h-[0.009rem] opacity-40 "
-             activeLineClassName="bg-white opacity-30"
-             placeholder={undefined}
-             onPointerEnterCapture={() => {}}
-             onPointerLeaveCapture={() => {}}
+            activeStep={activeStep}
+            isLastStep={(value) => setIsLastStep(value)}
+            isFirstStep={(value) => setIsFirstStep(value)}
+            lineClassName="bg-white/30 h-[0.009rem] opacity-40 "
+            activeLineClassName="bg-white opacity-30"
+            placeholder={undefined}
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
           >
-            <Step 
-              onClick={() => handleStepClick(0)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${0 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(0)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${0 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               1
             </Step>
-            <Step 
-              onClick={() => handleStepClick(1)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${1 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(1)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${1 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               2
             </Step>
-            <Step 
-              onClick={() => handleStepClick(2)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${2 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(2)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${2 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               3
             </Step>
-            <Step 
-              onClick={() => handleStepClick(3)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${3 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(3)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${3 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               4
             </Step>
-            <Step 
-              onClick={() => handleStepClick(4)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${4 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(4)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${4 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               5
             </Step>
-            <Step 
-              onClick={() => handleStepClick(5)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${5 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(5)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${5 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               6
             </Step>
-            <Step 
-              onClick={() => handleStepClick(6)} 
-              className={`z-10 relative bg-[#130a3d] border border-white/30 text-white ${6 <= highestStep + 1 ? "hover:bg-[#1e1252] cursor-pointer" : "cursor-not-allowed"}`}
+            <Step
+              onClick={() => handleStepClick(6)}
+              className={`relative z-10 border border-white/30 bg-[#130a3d] text-white ${6 <= highestStep + 1 ? "cursor-pointer hover:bg-[#1e1252]" : "cursor-not-allowed"}`}
               activeClassName="bg-white text-black hover:text-white"
               completedClassName="bg-white text-black hover:text-white"
               placeholder={undefined}
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
             >
               7
             </Step>
           </Stepper>
         </div>
       </div>
-      
+
       {activeStep === 0 && <Onboarding1 onNext={handleNext} />}
-      {activeStep === 1 && <Onboarding2 onNext={handleNext} onPrev={handlePrev} />}
-      {activeStep === 2 && <Onboarding3 onNext={handleNext} onPrev={handlePrev} />}
-      {activeStep === 3 && <Onboarding4 onNext={handleNext} onPrev={handlePrev} />}
-      {activeStep === 4 && <Onboarding5 onNext={handleNext} onPrev={handlePrev} />}
-      {activeStep === 5 && <Onboarding6 onNext={handleNext} onPrev={handlePrev} />}
+      {activeStep === 1 && (
+        <Onboarding2 onNext={handleNext} onPrev={handlePrev} />
+      )}
+      {activeStep === 2 && (
+        <Onboarding3 onNext={handleNext} onPrev={handlePrev} />
+      )}
+      {activeStep === 3 && (
+        <Onboarding4 onNext={handleNext} onPrev={handlePrev} />
+      )}
+      {activeStep === 4 && (
+        <Onboarding5 onNext={handleNext} onPrev={handlePrev} />
+      )}
+      {activeStep === 5 && (
+        <Onboarding6 onNext={handleNext} onPrev={handlePrev} />
+      )}
       {activeStep === 6 && <Onboarding7 onPrev={handlePrev} />}
     </div>
-  )
-}
+  );
+};
 
-export default OnboardingPage
+export default OnboardingPage;
