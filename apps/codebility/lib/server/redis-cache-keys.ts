c209aa6c9785dd25f62d2c03d4e605b1e7ea645b
JM-Flores <jmiguelflores19@gmail.com>
2025-05-26 20:27:44 +0800

    add redis cache and set cache keys

---
+++ apps/codebility/lib/server/redis-cache-keys.ts
@@ -0,0 +1,14 @@
+export const cacheKeys = {
+    codevs: {
+        members: 'cache:codevs:members',
+        inhouse: "cache:codevs:inhouse",
+        applicants: "cache:codevs:applicants"
+    },
+    dashboard: {
+        admin: 'cache:dashboard:admin'
+    },
+    projects: {
+        all: 'cache:projects:all',
+        portfolio: 'cache:projects:portfolio'
+    },
+}
