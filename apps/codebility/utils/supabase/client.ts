c322558c335430fbfe0199b684c36d8e161590f2
yyyort <itpahilgadev@gmail.com>
2025-05-26 19:28:36 +0800

    fix: supabase api initialization

--- apps/codebility/utils/supabase/client.ts
+++ apps/codebility/utils/supabase/client.ts
@@ -1,7 +1,18 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClientClientComponent = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+export const createClientClientComponent = () => {
+  // Check if environment variables exist
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Supabase environment variables are not set");
+  }
+
+  return createBrowserClient(
+    supabaseUrl,
+    supabaseKey,
   );
+
+}
+  
