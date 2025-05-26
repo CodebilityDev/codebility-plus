eb0d38fbecb0f8bbbf2c52a6e55dfa6240ab4969
JM-Flores <jmiguelflores19@gmail.com>
2025-05-26 20:40:17 +0800

    implement cache in codevs page

--- apps/codebility/app/home/interns/page.tsx
+++ apps/codebility/app/home/interns/page.tsx
@@ -1,4 +1,6 @@
 import { getCodevs } from "@/lib/server/codev.service";
+import { getOrSetCache } from "@/lib/server/redis-cache";
+import { cacheKeys } from "@/lib/server/redis-cache-keys";
 
 import CodevContainer from "./_components/CodevContainer";
 
@@ -6,7 +8,10 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function CodevsPage() {
-  const { data: interns, error } = await getCodevs({ filters: { role_id: 4 } });
+  const { data: interns, error } = await getOrSetCache(
+    cacheKeys.codevs.members,
+    () => getCodevs({ filters: { role_id: 4 } }),
+  );
 
   if (error) {
     return <div>Error fetching interns: {error.message}</div>;
