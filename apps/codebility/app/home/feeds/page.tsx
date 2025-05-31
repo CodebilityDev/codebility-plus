d9c95ab2239910757387ae1812dc005e822a8fa5
jamcap25 <capyjames25@gmail.com>
2025-05-31 10:09:17 +0800

    add feeds

--- apps/codebility/app/home/feeds/page.tsx
+++ apps/codebility/app/home/feeds/page.tsx
@@ -1,9 +1,27 @@
-import { Box } from "@/Components/shared/dashboard";
+"use client";
+
+import { useEffect, useState } from "react";
+import { useUserStore } from "@/store/codev-store";
 
 import CreatePost from "./_components/CreatePost";
 import Feed from "./_components/Feed";
+import { getUserRole } from "./action";
 
 export default function FeedsPage() {
+  const [isMentor, setIsMentor] = useState(false);
+  const { user } = useUserStore(); // Access user and hydrate function
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      if (!user) return;
+      const role = await getUserRole(user.role_id ?? null);
+      setIsMentor(role === "Intern");
+    };
+
+    if (user) {
+      fetchRole();
+    }
+  }, [user]);
   return (
     <>
       <div className="text-black-800 dark:text-white">
@@ -13,12 +31,11 @@ export default function FeedsPage() {
           Codebility community.
         </p>
       </div>
-      <Box className="text-black-800 min-h-screen dark:text-white">
-        <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8">
-          <div className="mb-6"></div>
-          <Feed />
-        </div>
-      </Box>
+      <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8">
+        {isMentor && <div className="mb-6">Hello</div>}
+
+        <Feed />
+      </div>
     </>
   );
 }
