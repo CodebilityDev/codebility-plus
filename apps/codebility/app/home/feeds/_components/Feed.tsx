237aafde502850f04742eb253ad23f7d03a3730b
JM-Flores <jmiguelflores19@gmail.com>
2025-06-08 18:41:08 +0800

    improve feeds page and connect to supabase

--- apps/codebility/app/home/feeds/_components/Feed.tsx
+++ apps/codebility/app/home/feeds/_components/Feed.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DefaultPagination from "@/Components/ui/pagination";
 
+import { getPosts, PostType } from "../_services/query";
 import Post from "./Post";
-import { samplePosts } from "./SampleData";
 
 export default function Feed({ isMentor }) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [posts, setPosts] = useState<PostType[]>([]);
+
   const postsPerPage = 6;
 
-  const totalPages = Math.ceil(samplePosts.length / postsPerPage);
+  const totalPages = Math.ceil(posts.length / postsPerPage);
   const startIndex = (currentPage - 1) * postsPerPage;
-  const currentPosts = samplePosts.slice(startIndex, startIndex + postsPerPage);
+  const currentPosts = posts.slice(startIndex, startIndex + postsPerPage);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const newPosts = await getPosts();
+      setPosts(newPosts);
+    }
+
+    fetchPosts();
+  }, []);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -30,17 +41,7 @@ export default function Feed({ isMentor }) {
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         {currentPosts.map((post) => (
-          <Post
-            key={post.id}
-            user={post.user}
-            userImage={post.userImage}
-            content={post.content}
-            timestamp={post.timestamp}
-            image={post.image}
-            reactions={post.reactions}
-            postUrl={post.postUrl}
-            isMentor={isMentor}
-          />
+          <Post post={post} isMentor={isMentor} />
         ))}
       </div>
       <DefaultPagination
