9cc3cf14923f5166b6e6a067c3f0b71707562853
JM-Flores <jmiguelflores19@gmail.com>
2025-06-08 18:41:08 +0800

    improve feeds page and connect to supabase

--- apps/codebility/app/home/feeds/_components/Post.tsx
+++ apps/codebility/app/home/feeds/_components/Post.tsx
@@ -1,89 +1,74 @@
 import Image from "next/image";
 import { Box } from "@/Components/shared/dashboard";
+import { defaultAvatar } from "@/public/assets/images";
+import { format } from "date-fns";
 import { ArrowBigUp } from "lucide-react";
 
+import { PostType } from "../_services/query";
+
 interface PostProps {
-  user: string;
-  userImage: string;
-  content: string;
-  timestamp: string;
-  image: string;
-  reactions: {
-    likes: number;
-  };
-  postUrl: string;
+  post: PostType;
   isMentor?: boolean;
 }
 
-export default function Post({
-  user,
-  userImage,
-  content,
-  timestamp,
-  image,
-  reactions,
-  postUrl,
-  isMentor,
-}: PostProps) {
+export default function Post({ post, isMentor }: PostProps) {
   return (
-    <Box className="group flex h-[350px] flex-col justify-between p-3">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          {isMentor && (
-            <button
-              className="invisible group-hover:visible"
-              onClick={() => console.log(`Delete post with ID: ${user}`)}
-            >
-              ✕
-            </button>
-          )}
-          <Image
-            src={userImage}
-            alt={`${user}'s profile`}
-            className="mr-4 h-10 w-10 rounded-full object-cover"
-            width={40}
-            height={40}
-          />
-          <div className="flex-1">
-            <h2 className="font-semibold text-gray-800 dark:text-gray-100">
-              {user}
-            </h2>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              {timestamp}
-            </span>
+    <Box className="group flex h-[400px] flex-col justify-between p-3">
+      <div className="mx-4 mt-2 h-[200px]">
+        <div className="flex items-center justify-between">
+          <div className="mb-2 flex items-center">
+            {isMentor && (
+              <button
+                className="invisible group-hover:visible"
+                onClick={() => console.log(`Delete post`)}
+              >
+                ✕
+              </button>
+            )}
+            {/* Author Image */}
+            <Image
+              src={post.author_id?.image_url || defaultAvatar}
+              alt={`${post.author_id?.first_name}'s profile`}
+              className="mr-4 h-12 w-12 rounded-full object-cover"
+              width={40}
+              height={40}
+            />
+            {/* Auth Name */}
+            <div className="flex-1">
+              <h2 className="font-semibold text-gray-800 dark:text-gray-100">
+                {post.author_id
+                  ? `${post.author_id?.first_name}  ${post.author_id?.last_name}`
+                  : "Codebility"}
+              </h2>
+            </div>
           </div>
         </div>
-        {/* Read Post Button */}
-        <a
-          href={postUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-black-300 invisible rounded-sm py-1 text-[10px] text-white hover:text-slate-200 group-hover:visible dark:bg-white dark:text-blue-400 dark:hover:text-blue-600"
-        >
-          Read Post
-        </a>
-      </div>
-      {/* Content */}
-      <div className="flex-grow">
-        <p className="mt-2 line-clamp-2 text-gray-700 dark:text-gray-300">
-          {content}
+        {/* Title */}
+        <div className="h-[60px] flex-grow">
+          <p className="mt-2 line-clamp-2 text-xl font-semibold text-gray-700 dark:text-gray-300">
+            {post.title}
+          </p>
+        </div>
+        {/* Date */}
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          {format(new Date("2025-01-15T10:30:00.000Z"), "MMM d, yyyy")}
         </p>
       </div>
       {/* Image */}
-      <div className="mt-4 h-40 w-full overflow-hidden rounded-sm">
+      <div className="h-60 w-full overflow-hidden rounded-sm">
         <img
-          src={image}
+          src={post.image_url || "/assets/images/bg-certificate.png"}
           alt="Post image"
-          className="h-40 w-full object-cover"
+          className="h-full w-full object-cover"
         />
       </div>
-      {/* Reactions */}
-      <div className="mt-4 flex items-center space-x-4 text-gray-600 dark:text-gray-400">
+      {/* Upvotes */}
+      <div className="mx-4  mt-4 flex items-center space-x-4 text-gray-600 dark:text-gray-400">
         <button className="flex items-center space-x-1 hover:text-blue-500 dark:hover:text-blue-400">
           <span>
             <ArrowBigUp />
           </span>
-          <span>{reactions.likes}</span>
+          <span>{post.upvoters_id ? post.upvoters_id.length : 0}</span>
         </button>
       </div>
     </Box>
--- apps/codebility/app/home/feeds/_services/action.ts
+++ apps/codebility/app/home/feeds/_services/action.ts
@@ -32,3 +32,38 @@ export const getUserRole = async (userId: Number | null): Promise<string | null>
 
   return role.name;
 };
+
+export const addPost = async () => {
+  try {
+    const supabase = await createClientServerComponent();
+
+    console.log("TEST 1");
+  
+    const { data: posts, error } = await supabase
+    .from("posts")
+    .select(`
+      *,
+      author_id(
+        first_name,
+        last_name,
+        image_url
+      )
+      `).order('created_at', { ascending: false });
+
+    if (error) throw error;
+
+    // console.log("Fetched posts:", posts);
+
+    return posts;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export type addPostType = {
+  title: string;
+  content: string;
+  author_id?: string;
+  image_url?: string;
+};
