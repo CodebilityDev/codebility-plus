72afff718427d96569750ab0109540fd8bd7d3ce
JM-Flores <jmiguelflores19@gmail.com>
2025-07-07 16:31:24 +0800

    add promote from codev to mentor feature

--- apps/codebility/app/home/_services/actions.ts
+++ apps/codebility/app/home/_services/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { createClientServerComponent } from "@/utils/supabase/server";
 
-export const declinePromotionToCodev = async (userId: string): Promise<void> => {
+export const declinePromotion = async (userId: string): Promise<void> => {
   if (!userId) {
     return;
   }
@@ -29,6 +29,24 @@ export const acceptPromotionToCodev = async (userId: string): Promise<void> => {
   const { error } = await supabase
     .from("codev")
     .update({ role_id: 10 , promote_declined: null})
+    .eq("id", userId);
+
+  if (error) {
+    console.error("Error updating role_id:", error);
+    throw error;
+  }
+};
+
+export const acceptPromotionToMentor = async (userId: string): Promise<void> => {
+  if (!userId) {
+    return;
+  }
+
+  const supabase = await createClientServerComponent();
+
+  const { error } = await supabase
+    .from("codev")
+    .update({ role_id: 5 , promote_declined: null})
     .eq("id", userId);
 
   if (error) {
