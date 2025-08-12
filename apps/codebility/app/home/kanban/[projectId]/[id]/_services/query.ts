8bd4931c0ad82fb265472220f8ac7a8dc57803a9
JM-Flores <jmiguelflores19@gmail.com>
2025-08-12 16:34:55 +0800

    create and use kanban store

---
+++ apps/codebility/app/home/kanban/[projectId]/[id]/_services/query.ts
@@ -0,0 +1,62 @@
+"use server"
+
+import { createClientServerComponent } from "@/utils/supabase/server";
+
+export const getBoardData = async (boardId: String) => {
+  try {
+    const supabase = await createClientServerComponent();
+  
+    const { data: boardData, error } = await supabase
+    .from("kanban_boards")
+    .select(
+      `
+      id,
+      name,
+      description,
+      created_at,
+      updated_at,
+      kanban_columns (
+        id,
+        name,
+        position,
+        created_at,
+        updated_at,
+        tasks (
+          id,
+          title,
+          description,
+          priority,
+          difficulty,
+          type,
+          due_date,
+          points,
+          pr_link,
+          sidekick_ids,
+          created_by,
+          kanban_column_id,
+          is_archive,
+          codev!tasks_codev_id_fkey (
+            id,
+            first_name,
+            last_name,
+            image_url
+          ),
+          skill_category!tasks_skill_category_id_fkey (
+            id,
+            name
+          )
+        )
+      )
+    `,
+    )
+    .eq("id", boardId)
+    .single();
+
+    if (error) throw error;
+
+    return boardData;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
