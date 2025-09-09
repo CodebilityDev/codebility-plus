16ded92f1fc2dc90ca6baf7765934ee1fa909a86
yyyort <itpahilgadev@gmail.com>
2025-09-09 16:18:40 +0800

    fix codev page missing import issue

--- apps/codebility/app/home/interns/_components/CodevContainer.tsx
+++ apps/codebility/app/home/interns/_components/CodevContainer.tsx
@@ -8,6 +8,7 @@ import CodevList from "./CodevList";
 import CodevSearchbar from "./CodevSearchbar";
 import FilterCodevs from "./FilterCodevs";
 import InternalProjects from "./InternalProjects";
+import { Tabs, TabsList, TabsTrigger } from "@codevs/ui/tabs";
 
 export default function CodevContainer({ data }: { data: Codev[] }) {
   const [filters, setFilters] = useState({
