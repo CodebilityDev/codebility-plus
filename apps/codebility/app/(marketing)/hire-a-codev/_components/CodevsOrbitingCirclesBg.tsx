c357defe869446eddbf135702ff4bc8154dabe26
meistergpc <jap1centeno@gmail.com>
2025-10-23 00:15:42 +0200

    feat(TechAnimation): increase icon size for improved visibility

--- apps/codebility/app/(marketing)/hire-a-codev/_components/CodevsOrbitingCirclesBg.tsx
+++ apps/codebility/app/(marketing)/hire-a-codev/_components/CodevsOrbitingCirclesBg.tsx
@@ -12,40 +12,60 @@ const OrbitingCirclesBackground = () => {
     <div className="absolute -right-72 z-0 hidden h-full w-full max-w-[60rem] items-center justify-center overflow-hidden rounded-lg md:flex md:shadow-xl">
       {/* Inner Circles */}
       <OrbitingCircles
-        className="h-[50px] w-[50px]"
+        className="h-[90px] w-[90px]"
         duration={20}
         delay={30}
         radius={150}
       >
-        <IconTailwind className="h-full w-full" />
+        <div
+          style={{ transform: "scale(5)" }}
+          className="flex items-center justify-center"
+        >
+          <IconTailwind />
+        </div>
       </OrbitingCircles>
       <OrbitingCircles
-        className="h-[50px] w-[50px]"
+        className="h-[90px] w-[90px]"
         duration={20}
         delay={20}
         radius={150}
       >
-        <IconNodeJS className="h-full w-full" />
+        <div
+          style={{ transform: "scale(4)" }}
+          className="flex items-center justify-center"
+        >
+          <IconNodeJS />
+        </div>
       </OrbitingCircles>
 
       {/* Outer Circles (reverse) */}
       <OrbitingCircles
-        className="h-[50px] w-[50px]"
+        className="!border-1 h-[90px] w-[90px]"
         radius={330}
         duration={20}
         delay={30}
         reverse
       >
-        <IconNextJS className="h-full w-full" />
+        <div
+          style={{ transform: "scale(5)" }}
+          className="flex items-center justify-center"
+        >
+          <IconNextJS />
+        </div>
       </OrbitingCircles>
       <OrbitingCircles
-        className="h-[50px] w-[50px]"
+        className="h-[90px] w-[90px]"
         radius={330}
         duration={20}
         delay={20}
         reverse
       >
-        <IconReact className="h-full w-full" />
+        <div
+          style={{ transform: "scale(3.5)" }}
+          className="flex items-center justify-center"
+        >
+          <IconReact />
+        </div>
       </OrbitingCircles>
     </div>
   );
